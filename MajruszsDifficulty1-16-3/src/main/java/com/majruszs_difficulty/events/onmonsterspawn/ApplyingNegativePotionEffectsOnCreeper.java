bf2a3f77e9938bbeb6ae8df6d6760841f6ae2425
Majrusz <mateuszsmolarkiewicz@wp.pl>
2020-12-26 20:15:51 +0100

    Update ApplyingNegativePotionEffectsOnCreeper.java

--- MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/events/onmonsterspawn/ApplyingNegativePotionEffectsOnCreeper.java
+++ MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/events/onmonsterspawn/ApplyingNegativePotionEffectsOnCreeper.java
@@ -10,10 +10,7 @@ import net.minecraft.potion.Effects;
 public class ApplyingNegativePotionEffectsOnCreeper {
 	protected static final double effectChance = 0.375D;
 	protected static final int effectDurationInTicks = MajruszsHelper.minutesToTicks( 5.0D );
-
-	protected static final Effect[] effects = new Effect[] {
-		Effects.WEAKNESS, Effects.SLOWNESS, Effects.MINING_FATIGUE, Effects.SATURATION
-	};
+	protected static final Effect[] effects = new Effect[]{ Effects.WEAKNESS, Effects.SLOWNESS, Effects.MINING_FATIGUE, Effects.SATURATION };
 
 	public static void tryToApply( CreeperEntity creeper ) {
 		if( MajruszsDifficulty.RANDOM.nextDouble() >= effectChance )
