d1a6bdbaa0fbb1550678e08fd2c4d5a45764799d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2020-12-26 17:59:04 +0100

    Removed redundancy

--- MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/events/onmonsterspawn/ApplyingNegativePotionEffectsOnCreeper.java
+++ MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/events/onmonsterspawn/ApplyingNegativePotionEffectsOnCreeper.java
@@ -3,6 +3,7 @@ package com.majruszs_difficulty.events.onmonsterspawn;
 import com.majruszs_difficulty.MajruszsDifficulty;
 import com.majruszs_difficulty.MajruszsHelper;
 import net.minecraft.entity.monster.CreeperEntity;
+import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
@@ -10,17 +11,15 @@ public class ApplyingNegativePotionEffectsOnCreeper {
 	protected static final double effectChance = 0.375D;
 	protected static final int effectDurationInTicks = MajruszsHelper.minutesToTicks( 5.0D );
 
-	protected static final EffectInstance[] effects = new EffectInstance[]{
-		new EffectInstance( Effects.WEAKNESS, effectDurationInTicks, 0 ),
-		new EffectInstance( Effects.SLOWNESS, effectDurationInTicks, 0 ),
-		new EffectInstance( Effects.MINING_FATIGUE, effectDurationInTicks, 0 ),
-		new EffectInstance( Effects.SATURATION, effectDurationInTicks, 0 )
+	protected static final Effect[] effects = new Effect[] {
+		Effects.WEAKNESS, Effects.SLOWNESS, Effects.MINING_FATIGUE, Effects.SATURATION
 	};
 
 	public static void tryToApply( CreeperEntity creeper ) {
 		if( MajruszsDifficulty.RANDOM.nextDouble() >= effectChance )
 			return;
 
-		creeper.addPotionEffect( effects[ MajruszsDifficulty.RANDOM.nextInt( effects.length ) ] );
+		Effect randomEffect = effects[ MajruszsDifficulty.RANDOM.nextInt( effects.length ) ];
+		creeper.addPotionEffect( new EffectInstance( randomEffect, effectDurationInTicks, 0 ) );
 	}
 }
