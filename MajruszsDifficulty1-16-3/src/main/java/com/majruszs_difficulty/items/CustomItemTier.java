c554ec934b550e3be79a187d4582ac6249966c02
Majrusz <mateuszsmolarkiewicz@wp.pl>
2020-12-26 22:54:54 +0100

    Added Wither Item Tier

---
+++ MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/items/CustomItemTier.java
@@ -0,0 +1,57 @@
+package com.majruszs_difficulty.items;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Items;
+import net.minecraft.item.crafting.Ingredient;
+
+import java.util.function.Supplier;
+
+public enum CustomItemTier implements IItemTier {
+	WITHER( 150, 5.0f, 3.0f, 3, 15, () -> Ingredient.fromItems( Items.BONE ) );
+
+	protected final int maxUses;
+	protected final float efficiency;
+	protected final float attackDamage;
+	protected final int harvestLevel;
+	protected final int enchantability;
+	protected final Supplier< Ingredient > repairMaterial;
+
+	CustomItemTier( int uses, float efficiency, float damage, int harvestLevel, int enchantability, Supplier< Ingredient > material ) {
+		this.maxUses = uses;
+		this.efficiency = efficiency;
+		this.attackDamage = damage;
+		this.harvestLevel = harvestLevel;
+		this.enchantability = enchantability;
+		this.repairMaterial = material;
+	}
+
+	@Override
+	public int getMaxUses() {
+		return this.maxUses;
+	}
+
+	@Override
+	public float getEfficiency() {
+		return this.efficiency;
+	}
+
+	@Override
+	public float getAttackDamage() {
+		return this.attackDamage;
+	}
+
+	@Override
+	public int getHarvestLevel() {
+		return this.harvestLevel;
+	}
+
+	@Override
+	public int getEnchantability() {
+		return this.enchantability;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial() {
+		return this.repairMaterial.get();
+	}
+}
