b9850a9bbcadd0a2bac9c78d6bc32b1f5a497dcc
Majrusz <mateuszsmolarkiewicz@wp.pl>
2020-12-26 20:16:16 +0100

    Refactoring

--- MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/DataSaver.java
+++ MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/DataSaver.java
@@ -3,6 +3,7 @@ package com.majruszs_difficulty;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.storage.WorldSavedData;
+import net.minecraftforge.event.world.WorldEvent;
 
 import java.util.function.Supplier;
 
@@ -32,5 +33,37 @@ public class DataSaver extends WorldSavedData implements Supplier {
 	@Override
 	public Object get() {
 		return this;
+	}
+
+	public static void onLoadingWorld( WorldEvent.Load event ) {
+		if( !( event.getWorld() instanceof ServerWorld ) )
+			return;
+
+		ServerWorld world = ( ServerWorld )event.getWorld();
+
+		if( world.isRemote )
+			return;
+
+		DataSaver saver = DataSaver.getDataFor( world );
+
+		if( saver.data.contains( "DifficultyState" ) ) {
+			GameState.changeMode( GameState.convertIntegerToMode( saver.data.getInt( "DifficultyState" ) ) );
+		}
+	}
+
+	public static void onSavingWorld( WorldEvent.Save event ) {
+		if( !( event.getWorld() instanceof ServerWorld ) )
+			return;
+
+		ServerWorld world = ( ServerWorld )event.getWorld();
+
+		if( world.isRemote )
+			return;
+
+		DataSaver saver = DataSaver.getDataFor( world );
+		CompoundNBT data = new CompoundNBT();
+		data.putInt( "DifficultyState", GameState.convertModeToInteger( GameState.getCurrentMode() ) );
+		saver.data = data;
+		saver.markDirty();
 	}
 }
