6234fdf13658bd44d3a2d9954396596d13adacc7
Majrusz <mateuszsmolarkiewicz@wp.pl>
2020-12-27 12:54:46 +0100

    Wither skeleton might spawn with wither sword which has enchantments

--- MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/events/onmonsterspawn/GiveWitherSkeletonSword.java
+++ MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/events/onmonsterspawn/GiveWitherSkeletonSword.java
@@ -1,12 +1,20 @@
 package com.majruszs_difficulty.events.onmonsterspawn;
 
+import com.majruszs_difficulty.MajruszsHelper;
 import com.majruszs_difficulty.RegistryHandler;
 import net.minecraft.entity.monster.WitherSkeletonEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
+import net.minecraft.world.server.ServerWorld;
 
 public class GiveWitherSkeletonSword {
 	public static void giveTo( WitherSkeletonEntity witherSkeleton ) {
-		witherSkeleton.setHeldItem( Hand.MAIN_HAND, new ItemStack( RegistryHandler.WITHER_SWORD.get() ) );
+		if( !witherSkeleton.isServerWorld() )
+			return;
+
+		double clampedRegionalDifficulty = MajruszsHelper.getClampedRegionalDifficulty( witherSkeleton, ( ServerWorld )witherSkeleton.world );
+		ItemStack sword = new ItemStack( RegistryHandler.WITHER_SWORD.get() );
+
+		witherSkeleton.setHeldItem( Hand.MAIN_HAND, MajruszsHelper.damageAndEnchantItemStack( sword, clampedRegionalDifficulty ) );
 	}
 }
