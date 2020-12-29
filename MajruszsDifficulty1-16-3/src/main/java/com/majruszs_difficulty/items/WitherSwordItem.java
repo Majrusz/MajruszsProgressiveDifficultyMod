ff6a753e7d38d7db1c61dd4755f0032536a98c66
Majrusz <mateuszsmolarkiewicz@wp.pl>
2020-12-29 19:12:40 +0100

    Added ToolTip to Wither Sword

--- MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/items/WitherSwordItem.java
+++ MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/items/WitherSwordItem.java
@@ -2,6 +2,7 @@ package com.majruszs_difficulty.items;
 
 import com.majruszs_difficulty.MajruszsHelper;
 import com.majruszs_difficulty.RegistryHandler;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,9 +10,18 @@ import net.minecraft.item.SwordItem;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 @Mod.EventBusSubscriber
 public class WitherSwordItem extends SwordItem {
@@ -33,5 +43,11 @@ public class WitherSwordItem extends SwordItem {
 
 			target.addPotionEffect( new EffectInstance( Effects.WITHER, witherDuration, 1 ) );
 		}
+	}
+
+	@Override
+	@OnlyIn( Dist.CLIENT )
+	public void addInformation( ItemStack stack, @Nullable World world, List< ITextComponent > toolTip, ITooltipFlag flag ) {
+		toolTip.add( new TranslationTextComponent( "item.majruszs_difficulty.wither_sword.tooltip" ).func_240699_a_( TextFormatting.GRAY ) );
 	}
 }
