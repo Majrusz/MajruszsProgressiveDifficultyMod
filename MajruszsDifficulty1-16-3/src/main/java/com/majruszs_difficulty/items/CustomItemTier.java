ec95007d65d79c162135c054aff0de2f89b179ae
Majrusz <mateuszsmolarkiewicz@wp.pl>
2020-12-30 18:00:30 +0100

    Refactoring

--- MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/items/CustomItemTier.java
+++ MajruszsDifficulty1-16-3/src/main/java/com/majruszs_difficulty/items/CustomItemTier.java
@@ -7,21 +7,18 @@ import net.minecraft.item.crafting.Ingredient;
 import java.util.function.Supplier;
 
 public enum CustomItemTier implements IItemTier {
-	WITHER( 250, 5.0f, 3.0f, 3, 15, () -> Ingredient.fromItems( Items.BONE ) );
+	WITHER( 250, 3, 15, 5.0f, 3.0f, () -> Ingredient.fromItems( Items.BONE ) );
 
-	protected final int maxUses;
-	protected final float efficiency;
-	protected final float attackDamage;
-	protected final int harvestLevel;
-	protected final int enchantability;
+	protected final int maxUses, harvestLevel, enchantability;
+	protected final float efficiency, attackDamage;
 	protected final Supplier< Ingredient > repairMaterial;
 
-	CustomItemTier( int uses, float efficiency, float damage, int harvestLevel, int enchantability, Supplier< Ingredient > material ) {
+	CustomItemTier( int uses, int harvestLevel, int enchantability, float efficiency, float damage, Supplier< Ingredient > material ) {
 		this.maxUses = uses;
-		this.efficiency = efficiency;
-		this.attackDamage = damage;
 		this.harvestLevel = harvestLevel;
 		this.enchantability = enchantability;
+		this.efficiency = efficiency;
+		this.attackDamage = damage;
 		this.repairMaterial = material;
 	}
 
