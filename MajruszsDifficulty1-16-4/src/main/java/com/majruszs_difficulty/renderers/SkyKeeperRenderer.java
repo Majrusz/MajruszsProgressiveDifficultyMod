849a2bb2f14c59a8dc31c7bc638e4368bc0cf430
Majrusz <majrusz@users.noreply.github.com>
2021-01-08 16:06:26 +0100

    Added sky keeper eyes to sky keeper renderer

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/renderers/SkyKeeperRenderer.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/renderers/SkyKeeperRenderer.java
@@ -1,6 +1,6 @@
 package com.majruszs_difficulty.renderers;
 
-import com.majruszs_difficulty.MajruszsDifficulty;
+import com.majruszs_difficulty.MajruszsHelper;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.PhantomRenderer;
 import net.minecraft.entity.monster.PhantomEntity;
@@ -8,13 +8,15 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
+/** Renderer for Sky Keeper entity. */
 @OnlyIn( Dist.CLIENT )
 public class SkyKeeperRenderer extends PhantomRenderer {
-	private static final ResourceLocation SKY_KEEPER_TEXTURE = new ResourceLocation( MajruszsDifficulty.MOD_ID, "textures/entity/sky_keeper.png" );
+	private static final ResourceLocation SKY_KEEPER_TEXTURE = MajruszsHelper.getResource( "textures/entity/sky_keeper.png" );
 
 	public SkyKeeperRenderer( EntityRendererManager renderManagerIn ) {
 		super( renderManagerIn );
 		this.layerRenderers.clear();
+		this.layerRenderers.add( new SkyKeeperEyesLayer<>( this ) );
 	}
 
 	public ResourceLocation getEntityTexture( PhantomEntity entity ) {
