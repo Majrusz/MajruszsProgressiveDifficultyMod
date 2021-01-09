b1be8c2f05cbc372ffe8a9148c9fb10718940847
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-09 20:32:27 +0100

    Refactoring OnAttackBase

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/events/attack_effects/OnAttackBase.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/events/attack_effects/OnAttackBase.java
@@ -1,33 +1,32 @@
 package com.majruszs_difficulty.events.attack_effects;
 
 import com.majruszs_difficulty.GameState;
-import com.majruszs_difficulty.GameState.*;
+import com.majruszs_difficulty.GameState.Mode;
 import com.majruszs_difficulty.MajruszsDifficulty;
 import com.majruszs_difficulty.MajruszsHelper;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.monster.SpiderEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
-import net.minecraft.util.DamageSource;
 import net.minecraft.world.server.ServerWorld;
 
 /** Base attack class representing event on which enemies will */
 public abstract class OnAttackBase< ClassType extends LivingEntity > {
 	protected final Class< ClassType > entityCausingEffect;
 	protected final Mode minimumMode;
-	protected final boolean shouldBeMultipliedByClampedRegionalDifficulty;
+	protected final boolean shouldBeMultipliedByCRD; // CRD = Clamped Regional Difficulty
 	protected final Effect[] effects;
 
-	public OnAttackBase( Class< ClassType > entityCausingEffect, Mode minimumMode, boolean shouldBeMultipliedByClampedRegionalDifficulty, Effect[] effects ) {
+	public OnAttackBase( Class< ClassType > entityCausingEffect, Mode minimumMode, boolean shouldBeMultipliedByCRD, Effect[] effects
+	) {
 		this.entityCausingEffect = entityCausingEffect;
 		this.minimumMode = minimumMode;
-		this.shouldBeMultipliedByClampedRegionalDifficulty = shouldBeMultipliedByClampedRegionalDifficulty;
+		this.shouldBeMultipliedByCRD = shouldBeMultipliedByCRD;
 		this.effects = effects;
 	}
 
-	public OnAttackBase( Class< ClassType > entityCausingEffect, Mode minimumMode, boolean shouldBeMultipliedByClampedRegionalDifficulty, Effect effect ) {
-		this( entityCausingEffect, minimumMode, shouldBeMultipliedByClampedRegionalDifficulty, new Effect[]{ effect } );
+	public OnAttackBase( Class< ClassType > entityCausingEffect, Mode minimumMode, boolean shouldBeMultipliedByCRD, Effect effect
+	) {
+		this( entityCausingEffect, minimumMode, shouldBeMultipliedByCRD, new Effect[]{ effect } );
 	}
 
 	/** Trying to apply all negative effects on entity. */
@@ -75,9 +74,6 @@ public abstract class OnAttackBase< ClassType extends LivingEntity > {
 
 	/** Calculating final effect chance. (after applying clamped regional difficulty if needed) */
 	private double calculateChance( LivingEntity attacker, ServerWorld world ) {
-		if( this.shouldBeMultipliedByClampedRegionalDifficulty )
-			return getChance() * MajruszsHelper.getClampedRegionalDifficulty( attacker, world );
-		else
-			return getChance();
+		return getChance() * ( this.shouldBeMultipliedByCRD ? MajruszsHelper.getClampedRegionalDifficulty( attacker, world ) : 1.0 );
 	}
 }
