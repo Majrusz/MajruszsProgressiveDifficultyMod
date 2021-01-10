117088d3ae4f9618c73c172d80ef6ce8630ac385
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-10 17:49:08 +0100

    Update AttributeHelper.java

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/AttributeHelper.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/AttributeHelper.java
@@ -3,23 +3,12 @@ package com.majruszs_difficulty;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.attributes.Attribute;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.attributes.ModifiableAttributeInstance;
 
 import java.util.UUID;
 
+/** Handling attributes in more user-friendly way. */
 public class AttributeHelper {
-	// This is done because the attribute names change between versions and are sometimes fields of type 'field_523832968'
-	public static class Attributes {
-		public static final Attribute MAX_HEALTH = net.minecraft.entity.ai.attributes.Attributes.MAX_HEALTH;
-		public static final Attribute ATTACK_DAMAGE = net.minecraft.entity.ai.attributes.Attributes.ATTACK_DAMAGE;
-		public static final Attribute ATTACK_KNOCKBACK = net.minecraft.entity.ai.attributes.Attributes.ATTACK_KNOCKBACK;
-		public static final Attribute FOLLOW_RANGE = net.minecraft.entity.ai.attributes.Attributes.FOLLOW_RANGE;
-		public static final Attribute MOVEMENT_SPEED = net.minecraft.entity.ai.attributes.Attributes.MOVEMENT_SPEED;
-		public static final Attribute KNOCKBACK_RESISTANCE = net.minecraft.entity.ai.attributes.Attributes.KNOCKBACK_RESISTANCE;
-		public static final Attribute ARMOR = net.minecraft.entity.ai.attributes.Attributes.ARMOR;
-		public static final Attribute ZOMBIE_REINFORCEMENT_CHANCE = net.minecraft.entity.ai.attributes.Attributes.ZOMBIE_SPAWN_REINFORCEMENTS;
-	}
 	private final UUID uuid;
 	private final String name;
 	private final Attribute attribute;
@@ -33,12 +22,18 @@ public class AttributeHelper {
 		this.operation = operation;
 	}
 
+	/** Setting current attribute value. */
 	public AttributeHelper setValue( double value ) {
 		this.value = value;
 
 		return this;
 	}
 
+	/**
+	 Applying current effect to the target.
+
+	 @param livingEntity Entity to apply effects.
+	 */
 	public AttributeHelper apply( LivingEntity livingEntity ) {
 		ModifiableAttributeInstance attributeInstance = livingEntity.getAttribute( this.attribute );
 
@@ -49,5 +44,17 @@ public class AttributeHelper {
 		}
 
 		return this;
+	}
+
+	// This is done because the attribute names change between versions and are sometimes fields of type 'field_523832968'
+	public static class Attributes {
+		public static final Attribute MAX_HEALTH = net.minecraft.entity.ai.attributes.Attributes.MAX_HEALTH;
+		public static final Attribute ATTACK_DAMAGE = net.minecraft.entity.ai.attributes.Attributes.ATTACK_DAMAGE;
+		public static final Attribute ATTACK_KNOCKBACK = net.minecraft.entity.ai.attributes.Attributes.ATTACK_KNOCKBACK;
+		public static final Attribute FOLLOW_RANGE = net.minecraft.entity.ai.attributes.Attributes.FOLLOW_RANGE;
+		public static final Attribute MOVEMENT_SPEED = net.minecraft.entity.ai.attributes.Attributes.MOVEMENT_SPEED;
+		public static final Attribute KNOCKBACK_RESISTANCE = net.minecraft.entity.ai.attributes.Attributes.KNOCKBACK_RESISTANCE;
+		public static final Attribute ARMOR = net.minecraft.entity.ai.attributes.Attributes.ARMOR;
+		public static final Attribute ZOMBIE_REINFORCEMENT_CHANCE = net.minecraft.entity.ai.attributes.Attributes.ZOMBIE_SPAWN_REINFORCEMENTS;
 	}
 }
