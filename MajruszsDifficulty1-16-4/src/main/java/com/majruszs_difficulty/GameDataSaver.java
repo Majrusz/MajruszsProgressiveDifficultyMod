b8733cf63ea8e7d2350a875339308deb87dc6027
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-10 17:44:50 +0100

    Update GameDataSaver.java

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/GameDataSaver.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/GameDataSaver.java
@@ -3,6 +3,7 @@ package com.majruszs_difficulty;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.world.storage.WorldSavedData;
 
+/** Saving current data like current game state. */
 public class GameDataSaver extends WorldSavedData {
 	public static final String DATA_NAME = MajruszsDifficulty.MOD_ID;
 	private CompoundNBT data = new CompoundNBT();
