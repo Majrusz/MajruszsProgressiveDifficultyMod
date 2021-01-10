12d58860a650eb9fd9c9e845349eac0ef490d0fc
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-10 14:15:26 +0100

    Changes in spider poison attack

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/events/attack_effects/SpiderPoisonOnAttack.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/events/attack_effects/SpiderPoisonOnAttack.java
@@ -7,6 +7,7 @@ import com.majruszs_difficulty.MajruszsHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.SpiderEntity;
+import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
@@ -16,6 +17,32 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+public class SpiderPoisonOnAttack extends OnAttackBase {
+	public SpiderPoisonOnAttack() {
+		super( SpiderEntity.class, GameState.Mode.EXPERT, true, Effects.POISON );
+	}
+
+	@Override
+	protected boolean isEnabled() {
+		return false;
+	}
+
+	@Override
+	protected double getChance() {
+		return 0;
+	}
+
+	@Override
+	protected int getDurationInTicks() {
+		return 0;
+	}
+
+	@Override
+	protected int getAmplifier() {
+		return 0;
+	}
+}
+/*
 @Mod.EventBusSubscriber
 public class SpiderPoisonAttack {
 	@SubscribeEvent
@@ -62,3 +89,4 @@ public class SpiderPoisonAttack {
 		return MajruszsHelper.getClampedRegionalDifficulty( target, world ) * Config.getChance( Config.Chances.SPIDER_POISON );
 	}
 }
+*/
