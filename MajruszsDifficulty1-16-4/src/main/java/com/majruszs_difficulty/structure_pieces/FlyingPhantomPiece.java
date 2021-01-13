4dc4c6e848c1ede25f8e8416261db6fb7a26829f
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-13 12:31:10 +0100

    Refactoring

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/structure_pieces/FlyingPhantomPiece.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/structure_pieces/FlyingPhantomPiece.java
@@ -1,19 +1,13 @@
 package com.majruszs_difficulty.structure_pieces;
 
-import com.majruszs_difficulty.MajruszsDifficulty;
 import com.majruszs_difficulty.MajruszsHelper;
 import com.majruszs_difficulty.RegistryHandler;
 import com.majruszs_difficulty.entities.SkyKeeperEntity;
-import com.majruszs_difficulty.structures.FlyingPhantomStructure;
-import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
-import net.minecraft.loot.LootTables;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ChestTileEntity;
-import net.minecraft.tileentity.LockableLootTileEntity;
 import net.minecraft.tileentity.MobSpawnerTileEntity;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Direction;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Rotation;
@@ -31,77 +25,62 @@ import java.util.Random;
 
 public class FlyingPhantomPiece extends TemplateStructurePiece {
 	public static final ResourceLocation resourceLocation = MajruszsHelper.getResource( "flying_phantom" );
+	public static final ResourceLocation chestResourceLocation = MajruszsHelper.getResource( "gameplay/fishing_treasure_loot" );
 	private final Rotation rotation;
 
-	public FlyingPhantomPiece( TemplateManager templateManagerIn, BlockPos pos, Rotation rotationIn ) {
+	public FlyingPhantomPiece( TemplateManager templateManager, BlockPos position, Rotation rotation ) {
 		super( RegistryHandler.FLYING_PHANTOM_PIECE, 0 );
-		this.templatePosition = pos;
-		this.rotation = rotationIn;
-		this.setupPiece( templateManagerIn );
+		this.templatePosition = position;
+		this.rotation = rotation;
+		this.setupPiece( templateManager );
 	}
 
-	public FlyingPhantomPiece( TemplateManager templateManagerIn, CompoundNBT tagCompound ) {
-		super( RegistryHandler.FLYING_PHANTOM_PIECE, tagCompound );
-		this.rotation = Rotation.valueOf( tagCompound.getString( "Rot" ) );
-		this.setupPiece( templateManagerIn );
-	}
-
-	@Override
-	protected void readAdditional( CompoundNBT tagCompound ) {
-		super.readAdditional( tagCompound );
-		tagCompound.putString( "Rot", this.rotation.name() );
+	public FlyingPhantomPiece( TemplateManager templateManager, CompoundNBT compoundNBT ) {
+		super( RegistryHandler.FLYING_PHANTOM_PIECE, compoundNBT );
+		this.rotation = Rotation.valueOf( compoundNBT.getString( "Rotation" ) );
+		this.setupPiece( templateManager );
 	}
 
 	@Override
-	protected void handleDataMarker( String function, BlockPos pos, IServerWorld worldIn, Random rand, MutableBoundingBox sbb ) {
+	protected void readAdditional( CompoundNBT compoundNBT ) {
+		super.readAdditional( compoundNBT );
+
+		compoundNBT.putString( "Rotation", this.rotation.name() );
+	}
+
+	@Override
+	protected void handleDataMarker( String function, BlockPos position, IServerWorld world, Random random, MutableBoundingBox boundingBox ) {
 		if( function.startsWith( "chest" ) ) {
-			TileEntity tileEntity = worldIn.getTileEntity( pos.down() );
+			TileEntity tileEntity = world.getTileEntity( position.down() );
 
-			// Just another check to make sure everything is going well before we try to set the chest.
-			if( tileEntity instanceof ChestTileEntity ) {
-				// tileentity.rotate( this.rotation );
-				// ((ChestTileEntity) tileentity).setLootTable(<resource_location_to_loottable>, rand.nextLong());
-			}
+			if( tileEntity instanceof ChestTileEntity )
+				( ( ChestTileEntity )tileEntity ).setLootTable( chestResourceLocation, random.nextLong() );
+
 		} else if( function.startsWith( "spawner" ) ) {
-			TileEntity tileEntity = worldIn.getTileEntity( pos.down() );
+			TileEntity tileEntity = world.getTileEntity( position.down() );
 
-			// Just another check to make sure everything is going well before we try to set the chest.
 			if( tileEntity instanceof MobSpawnerTileEntity ) {
 				EntityType< ? > entityType = function.equals( "spawner_keeper" ) ? SkyKeeperEntity.type : EntityType.PHANTOM;
 
-				( ( MobSpawnerTileEntity )tileEntity ).getSpawnerBaseLogic().setEntityType( entityType );
-				// ( ( MobSpawnerTileEntity )tileEntity ).getSpawnerBaseLogic().getCachedEntity()
-
-
-				// ((ChestTileEntity) tileentity).setLootTable(<resource_location_to_loottable>, rand.nextLong());
+				( ( MobSpawnerTileEntity )tileEntity ).getSpawnerBaseLogic()
+					.setEntityType( entityType );
 			}
 		}
 	}
 
-	/*
-	 * Begins assembling your structure and where the pieces needs to go.
-	 */
-	public static void start( TemplateManager templateManager, BlockPos pos, Rotation rotation, List< StructurePiece > pieceList, Random random ) {
-		int x = pos.getX();
-		int z = pos.getZ();
-
-		// This is how we factor in rotation for multi-piece structures.
-		//
-		// I would recommend using the OFFSET map above to have each piece at correct height relative of each other
-		// and keep the X and Z equal to 0. And then in rotations, have the centermost piece have a rotation
-		// of 0, 0, 0 and then have all other pieces' rotation be based off of the bottommost left corner of
-		// that piece (the corner that is smallest in X and Z).
-		//
-		// Lots of trial and error may be needed to get this right for your structure.
+	/** Begins assembling your structure and where the pieces needs to go. */
+	public static void start( TemplateManager templateManager, BlockPos position, Rotation rotation, List< StructurePiece > pieces, Random random ) {
 		BlockPos rotationOffSet = new BlockPos( 0, 0, 0 ).rotate( rotation );
-		BlockPos blockpos = rotationOffSet.add( x, pos.getY(), z );
-		pieceList.add( new FlyingPhantomPiece( templateManager, blockpos, rotation ) );
+		BlockPos blockpos = rotationOffSet.add( position.getX(), position.getY(), position.getZ() );
+
+		pieces.add( new FlyingPhantomPiece( templateManager, blockpos, rotation ) );
 	}
 
 	private void setupPiece( TemplateManager templateManager ) {
 		Template template = templateManager.getTemplateDefaulted( resourceLocation );
 		PlacementSettings placementsettings = ( new PlacementSettings() ).setRotation( this.rotation )
 			.setMirror( Mirror.NONE );
+
 		this.setup( template, this.templatePosition, placementsettings );
 	}
 }
--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/structures/FlyingPhantomStructure.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/structures/FlyingPhantomStructure.java
@@ -3,16 +3,12 @@ package com.majruszs_difficulty.structures;
 import com.google.common.collect.ImmutableList;
 import com.majruszs_difficulty.MajruszsDifficulty;
 import com.majruszs_difficulty.MajruszsHelper;
-import com.majruszs_difficulty.RegistryHandler;
 import com.majruszs_difficulty.entities.SkyKeeperEntity;
 import com.majruszs_difficulty.structure_pieces.FlyingPhantomPiece;
-import com.mojang.serialization.Codec;
-import net.minecraft.entity.EntityType;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MutableBoundingBox;
 import net.minecraft.util.registry.DynamicRegistries;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.MobSpawnInfo;
@@ -21,8 +17,8 @@ import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraft.world.gen.feature.StructureFeature;
-import net.minecraft.world.gen.feature.jigsaw.JigsawManager;
-import net.minecraft.world.gen.feature.structure.*;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 import net.minecraft.world.gen.settings.StructureSeparationSettings;
 
@@ -30,27 +26,27 @@ import java.util.List;
 
 public class FlyingPhantomStructure extends Structure< NoFeatureConfig > {
 	public static final FlyingPhantomStructure INSTANCE = new FlyingPhantomStructure();
-	public static final String NAME = MajruszsHelper.getResource( "flying_phantom_structure" ).toString();
-	public static final StructureFeature< NoFeatureConfig, ? extends Structure< NoFeatureConfig > > FEATURE =
-		WorldGenRegistries.register( WorldGenRegistries.CONFIGURED_STRUCTURE_FEATURE, NAME, INSTANCE.withConfiguration( NoFeatureConfig.field_236559_b_ ) );
-	public static final StructureSeparationSettings SEPARATION_SETTINGS = new StructureSeparationSettings(37, 27, 1717171717 );
+	public static final String NAME = MajruszsHelper.getResource( "flying_phantom_structure" )
+		.toString();
+	public static final StructureFeature< NoFeatureConfig, ? extends Structure< NoFeatureConfig > > FEATURE = WorldGenRegistries.register(
+		WorldGenRegistries.CONFIGURED_STRUCTURE_FEATURE, NAME, INSTANCE.withConfiguration( NoFeatureConfig.field_236559_b_ ) );
+	public static final StructureSeparationSettings SEPARATION_SETTINGS = new StructureSeparationSettings( 37, 27, 1717171717 );
 
 	private static final List< MobSpawnInfo.Spawners > STRUCTURE_MONSTERS = ImmutableList.of(
-		new MobSpawnInfo.Spawners( SkyKeeperEntity.type, 1, 3, 4 )
-	);
+		new MobSpawnInfo.Spawners( SkyKeeperEntity.type, 1, 1, 1 ) );
 
 	public FlyingPhantomStructure() {
 		super( NoFeatureConfig.field_236558_a_ );
-	}
-
-	public Structure.IStartFactory< NoFeatureConfig > getStartFactory() {
-		return FlyingPhantomStructure.Start::new;
 	}
 
 	/** Generation stage for where to generate the structure. */
 	@Override
 	public GenerationStage.Decoration getDecorationStage() {
 		return GenerationStage.Decoration.SURFACE_STRUCTURES;
+	}
+
+	public Structure.IStartFactory< NoFeatureConfig > getStartFactory() {
+		return FlyingPhantomStructure.Start::new;
 	}
 
 	/** Enemies spawning naturally near the structure. */
@@ -60,31 +56,30 @@ public class FlyingPhantomStructure extends Structure< NoFeatureConfig > {
 	}
 
 	public static class Start extends StructureStart< NoFeatureConfig > {
-		public Start( Structure< NoFeatureConfig > structure, int chunkX, int chunkZ, MutableBoundingBox mutableBoundingBox, int reference, long seed ) {
+		public Start( Structure< NoFeatureConfig > structure, int chunkX, int chunkZ, MutableBoundingBox mutableBoundingBox, int reference,
+			long seed
+		) {
 			super( structure, chunkX, chunkZ, mutableBoundingBox, reference, seed );
 		}
 
 		@Override
-		public void func_230364_a_( DynamicRegistries dynamicRegistryManager, ChunkGenerator chunkGenerator, TemplateManager templateManagerIn, int chunkX, int chunkZ, Biome biomeIn, NoFeatureConfig config) {
-			Rotation rotation = Rotation.values()[this.rand.nextInt(Rotation.values().length)];
+		public void func_230364_a_( DynamicRegistries dynamicRegistryManager, ChunkGenerator chunkGenerator, TemplateManager templateManager,
+			int chunkX, int chunkZ, Biome biome, NoFeatureConfig config
+		) {
+			Rotation rotation = Rotation.values()[ this.rand.nextInt( Rotation.values().length ) ];
 
 			// Turns the chunk coordinates into actual coordinates we can use. (Gets center of that chunk)
-			int x = (chunkX << 4) + 7;
-			int z = (chunkZ << 4) + 7;
+			int x = ( chunkX << 4 ) + 7;
+			int z = ( chunkZ << 4 ) + 7;
+			int y = chunkGenerator.getHeight( x, z, Heightmap.Type.WORLD_SURFACE_WG ) + 60 + MajruszsDifficulty.RANDOM.nextInt( 60 );
 
-			// Finds the y value of the terrain at location.
-			int surfaceY = chunkGenerator.getHeight(x, z, Heightmap.Type.WORLD_SURFACE_WG);
-			BlockPos blockpos = new BlockPos(x, surfaceY + 100, z);
+			BlockPos blockpos = new BlockPos( x, y, z );
 
-			// Now adds the structure pieces to this.components with all details such as where each part goes
-			// so that the structure can be added to the world by worldgen.
-			FlyingPhantomPiece.start( templateManagerIn, blockpos, rotation, this.components, this.rand );
+			FlyingPhantomPiece.start( templateManager, blockpos, rotation, this.components, this.rand );
 
-			// Sets the bounds of the structure.
 			this.recalculateStructureSize();
 
-			// I use to debug and quickly find out if the structure is spawning or not and where it is.
-			MajruszsDifficulty.LOGGER.info( "Rundown House at " + (blockpos.getX()) + " " + blockpos.getY() + " " + (blockpos.getZ()));
+			MajruszsDifficulty.LOGGER.info( "Phantom at " + blockpos.getX() + " " + blockpos.getY() + " " + blockpos.getZ() );
 		}
 	}
 }
