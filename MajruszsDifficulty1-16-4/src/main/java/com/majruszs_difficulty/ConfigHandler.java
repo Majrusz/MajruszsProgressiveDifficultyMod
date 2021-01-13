0d20ad6260b7d8718ac557f7c3f90b1b34b243c1
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-13 14:33:31 +0100

    Increased Sky Keeper levitation duration

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/ConfigHandler.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/ConfigHandler.java
@@ -110,7 +110,7 @@ public class ConfigHandler {
 		Config.Durations.ARROW_BLEEDING = createConfigSpecForDouble( "arrow_bleeding_duration", "", 24.0, 1.0, 600.0 );
 		Config.Durations.THROWN_TRIDENT_BLEEDING = createConfigSpecForDouble( "thrown_trident_bleeding_duration", "", 30.0, 1.0, 600.0 );
 		Config.Durations.BITE_BLEEDING = createConfigSpecForDouble( "bite_bleeding_duration", "", 24.0, 1.0, 600.0 );
-		Config.Durations.SKY_KEEPER_LEVITATION = createConfigSpecForDouble( "sky_keeper_levitation_duration", "", 5.0, 1.0, 600.0 );
+		Config.Durations.SKY_KEEPER_LEVITATION = createConfigSpecForDouble( "sky_keeper_levitation_duration", "", 6.0, 1.0, 600.0 );
 		BUILDER.pop();
 
 		BUILDER.push( "Values" );
