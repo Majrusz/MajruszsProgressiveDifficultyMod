efd091d76d87abffda5b8e93ebd1ddc24deaba63
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-16 21:20:09 +0100

    Added seperate texture for Giant

--- MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/renderers/GiantRenderer.java
+++ MajruszsDifficulty1-16-4/src/main/java/com/majruszs_difficulty/renderers/GiantRenderer.java
@@ -1,16 +1,20 @@
 package com.majruszs_difficulty.renderers;
 
+import com.majruszs_difficulty.MajruszsHelper;
 import com.majruszs_difficulty.entities.GiantEntity;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.ZombieRenderer;
 import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 /** Renderer for Giant entity. */
 @OnlyIn( Dist.CLIENT )
 public class GiantRenderer extends ZombieRenderer {
+	private static final ResourceLocation TEXTURE = MajruszsHelper.getResource( "textures/entity/giant.png" );
+
 	public GiantRenderer( EntityRendererManager renderManagerIn ) {
 		super( renderManagerIn );
 		this.shadowSize = 0.375f * GiantEntity.scale;
@@ -19,5 +23,10 @@ public class GiantRenderer extends ZombieRenderer {
 	@Override
 	protected void preRenderCallback( ZombieEntity entity, MatrixStack matrixStackIn, float partialTickTime ) {
 		matrixStackIn.scale( GiantEntity.scale, GiantEntity.scale, GiantEntity.scale );
+	}
+
+	@Override
+	public ResourceLocation getEntityTexture( ZombieEntity entity ) {
+		return TEXTURE;
 	}
 }
