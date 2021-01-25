3d94d16947db312160c0ebe1bf9c247021b74a50
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-25 14:46:07 +0100

    Refactoring

--- src/main/java/com/majruszs_difficulty/events/SpawnDisabler.java
+++ src/main/java/com/majruszs_difficulty/events/SpawnDisabler.java
@@ -1,7 +1,6 @@
 package com.majruszs_difficulty.events;
 
 import com.majruszs_difficulty.GameState;
-import com.majruszs_difficulty.GameState.State;
 import com.majruszs_difficulty.Instances;
 import com.majruszs_difficulty.entities.EliteSkeletonEntity;
 import com.majruszs_difficulty.entities.GiantEntity;
@@ -10,26 +9,24 @@ import com.majruszs_difficulty.entities.SkyKeeperEntity;
 import com.mlib.config.AvailabilityConfig;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.IllusionerEntity;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.gen.feature.structure.Structure;
-import net.minecraft.world.raid.Raid;
-import net.minecraft.world.raid.RaidManager;
 import net.minecraft.world.server.ServerWorld;
-import net.minecraftforge.common.world.StructureSpawnManager;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+/** Disabling natural spawns for entities that have not met certain conditions. */
 @Mod.EventBusSubscriber
 public class SpawnDisabler {
 	@SubscribeEvent
 	public static void disableSpawns( LivingSpawnEvent.CheckSpawn event ) {
-		if( isEntityToBeDisabled( event.getEntityLiving() ) )
+		if( shouldEntitySpawnBeDisabled( event.getEntityLiving() ) )
 			event.setResult( Event.Result.DENY );
 	}
 
-	protected static boolean isEntityToBeDisabled( Entity entity ) {
+	/** Checks whether given entity should not spawn. */
+	protected static boolean shouldEntitySpawnBeDisabled( Entity entity ) {
 		boolean isGiant = entity instanceof GiantEntity;
 		boolean isIllusioner = entity instanceof IllusionerEntity;
 		boolean isPillagerWolf = entity instanceof PillagerWolfEntity;
@@ -50,16 +47,22 @@ public class SpawnDisabler {
 			return false;
 	}
 
+	/** Checks most common variant for disabling. */
 	private static boolean shouldBeDisabled( GameState.State minimumState, AvailabilityConfig config ) {
 		return !GameState.atLeast( minimumState ) || config.isDisabled();
 	}
 
+	/**
+	 Checks whether there is the village nearby.
+	 It is required to disable spawns near village for Illusioner because
+	 otherwise Illusioner may spawn underground while there is raid active and
+	 players will have a problem to finish it.
+	 */
 	private static boolean isVillageNearby( Entity entity ) {
 		if( !( entity.world instanceof ServerWorld ) )
 			return false;
 
 		ServerWorld world = ( ServerWorld )entity.world;
-
 		return world.func_241117_a_( Structure.VILLAGE, entity.getPosition(), 10000, false ) != null;
 	}
 }
