c396fe5dacc75c82466d8c41be1b5b3414d58dd7
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-01-29 23:23:56 +0100

    Added End Ingot

---
+++ src/main/java/com/majruszs_difficulty/items/EndIngotItem.java
@@ -0,0 +1,12 @@
+package com.majruszs_difficulty.items;
+
+import com.majruszs_difficulty.Instances;
+import net.minecraft.item.Item;
+import net.minecraft.item.Rarity;
+
+/** New strong end-game ingot. */
+public class EndIngotItem extends Item {
+	public EndIngotItem() {
+		super( new Properties().group( Instances.ITEM_GROUP ).rarity( Rarity.UNCOMMON ) );
+	}
+}
