f230d37fb9ccd062dcf80e11f70c39f8e60f822a
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-02-06 18:48:59 +0100

    Refactoring

--- src/main/java/com/majruszs_difficulty/events/undead_army/Direction.java
+++ src/main/java/com/majruszs_difficulty/events/undead_army/Direction.java
@@ -1,7 +1,9 @@
 package com.majruszs_difficulty.events.undead_army;
 
 import com.mlib.MajruszLibrary;
+import com.mlib.Random;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.server.ServerWorld;
 
@@ -9,21 +11,23 @@ import net.minecraft.world.server.ServerWorld;
 public enum Direction {
 	WEST( -1, 0 ), EAST( 1, 0 ), NORTH( 0, -1 ), SOUTH( 0, 1 );
 
-	private static final int standardDeviation = 10;
+	private static final int STANDARD_DEVIATION = 10;
 	public final int x, z;
+	private final int xFactor, zFactor;
 
 	Direction( int x, int z ) {
 		this.x = x;
 		this.z = z;
+		this.xFactor = ( this.z != 0 ? 5 : 1 ) * STANDARD_DEVIATION;
+		this.zFactor = ( this.x != 0 ? 5 : 1 ) * STANDARD_DEVIATION;
 	}
 
 	/** Random position for entity to spawn. */
 	public BlockPos getRandomSpawnPosition( ServerWorld world, BlockPos positionToAttack, int spawnRadius ) {
-		int xFactor = ( this.z != 0 ? 5 : 1 ) * standardDeviation;
-		int zFactor = ( this.x != 0 ? 5 : 1 ) * standardDeviation;
+		Vector3d offset = Random.getRandomVector3d( -this.xFactor, this.xFactor, 0.0, 0.0, -this.zFactor, this.zFactor );
 
-		int x = positionToAttack.getX() + this.x * spawnRadius + MajruszLibrary.RANDOM.nextInt( xFactor * 2 ) - xFactor;
-		int z = positionToAttack.getZ() + this.z * spawnRadius + MajruszLibrary.RANDOM.nextInt( zFactor * 2 ) - zFactor;
+		int x = positionToAttack.getX() + this.x * spawnRadius + ( int )offset.x;
+		int z = positionToAttack.getZ() + this.z * spawnRadius + ( int )offset.z;
 		int y = world.getHeight( Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, x, z ) + 1;
 
 		return new BlockPos( x, y, z );
