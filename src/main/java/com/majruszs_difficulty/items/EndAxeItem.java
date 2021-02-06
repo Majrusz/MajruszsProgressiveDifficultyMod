3a3ba14ccf0bec2a707aa9eb19c4bc9c1aea3b0a
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-02-06 17:16:40 +0100

    Increased End Axe damage and reduced attack speed

--- src/main/java/com/majruszs_difficulty/items/EndAxeItem.java
+++ src/main/java/com/majruszs_difficulty/items/EndAxeItem.java
@@ -6,7 +6,7 @@ import net.minecraft.item.AxeItem;
 /** New late game axe. */
 public class EndAxeItem extends AxeItem {
 	public EndAxeItem() {
-		super( CustomItemTier.END, 5.0f, -3.0f, ( new Properties() ).group( Instances.ITEM_GROUP )
+		super( CustomItemTier.END, 6.0f, -3.1f, ( new Properties() ).group( Instances.ITEM_GROUP )
 			.isImmuneToFire() );
 	}
 }
