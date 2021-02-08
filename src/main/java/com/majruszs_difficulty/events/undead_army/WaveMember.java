dd22506c02a463d141fa6f084e6d7e614d39c01e
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-02-08 15:28:16 +0100

    Removed Giant from Undead Army

--- src/main/java/com/majruszs_difficulty/events/undead_army/WaveMember.java
+++ src/main/java/com/majruszs_difficulty/events/undead_army/WaveMember.java
@@ -1,7 +1,6 @@
 package com.majruszs_difficulty.events.undead_army;
 
 import com.majruszs_difficulty.entities.EliteSkeletonEntity;
-import com.majruszs_difficulty.entities.GiantEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraftforge.common.IExtensibleEnum;
 
@@ -9,8 +8,8 @@ import java.util.Arrays;
 
 /** Amount of entities in waves. */
 public enum WaveMember implements IExtensibleEnum {
-	ZOMBIE( EntityType.ZOMBIE, 5, 4, 3, 2, 2 ), HUSK( EntityType.HUSK, 1, 1, 2, 3, 4 ), GIANT( GiantEntity.type, 0, 0, 0, 1, 2 ), SKELETON(
-		EntityType.SKELETON, 3, 3, 2, 2, 2 ), STRAY( EntityType.STRAY, 1, 1, 1, 2, 3 ), ELITE_SKELETON( EliteSkeletonEntity.type, 1, 2, 3, 4, 5 );
+	ZOMBIE( EntityType.ZOMBIE, 5, 4, 3, 2, 2 ), HUSK( EntityType.HUSK, 1, 1, 2, 4, 5 ), SKELETON(
+		EntityType.SKELETON, 3, 3, 2, 2, 2 ), STRAY( EntityType.STRAY, 1, 1, 2, 3, 4 ), ELITE_SKELETON( EliteSkeletonEntity.type, 1, 2, 3, 5, 6 );
 
 	public final EntityType< ? > type;
 	public final int[] waveCounts;
