ac4997546bbe00bdccccabc2aee53e04aa753f08
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-02-14 13:58:42 +0100

    Updated version name

--- src/main/java/com/majruszs_difficulty/MajruszsDifficulty.java
+++ src/main/java/com/majruszs_difficulty/MajruszsDifficulty.java
@@ -19,7 +19,7 @@ import java.util.function.Predicate;
 public class MajruszsDifficulty {
 	public static final String MOD_ID = "majruszs_difficulty";
 	public static final String NAME = "Majrusz's Progressive Difficulty";
-	public static final String VERSION = "0.5.0";
+	public static final String VERSION = "0.5.1";
 	public static final ConfigHandler CONFIG_HANDLER = new ConfigHandler( ModConfig.Type.COMMON, "majruszs-difficulty-common.toml" );
 	public static final ConfigGroup FEATURES_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Features", "" ) );
 	public static final ConfigGroup ENTITIES_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Entities", "" ) );
