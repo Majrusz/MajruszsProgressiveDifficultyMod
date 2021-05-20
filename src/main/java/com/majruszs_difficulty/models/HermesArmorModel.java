0b93e17658338bfa1c41dcaa61552d4aa64de8e3
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-05-20 08:56:01 +0200

    Fixed textures for Hermes Boots

--- src/main/java/com/majruszs_difficulty/models/HermesArmorModel.java
+++ src/main/java/com/majruszs_difficulty/models/HermesArmorModel.java
@@ -12,17 +12,17 @@ public class HermesArmorModel extends BipedModel< LivingEntity > {
 		super( 1.0f );
 
 		this.bipedLeftLeg.setTextureOffset( 16, 0 )
-			.addBox( 3.0f, 7.0f, 0.0f, 1.0f, 3.0f, 2.0f, 0.0f, 0.0f, 0.0f );
+			.addBox( 3.0f, 8.0f, 1.0f+0.0f, 1.0f, 3.0f, 2.0f, 0.0f, 0.0f, 0.0f );
 		this.bipedLeftLeg.setTextureOffset( 22, 0 )
-			.addBox( 3.0f, 7.0f, 2.0f, 1.0f, 2.0f, 1.0f, 0.0f, 0.0f, 0.0f );
-		this.bipedLeftLeg.setTextureOffset( 0, 10 )
-			.addBox( 3.0f, 7.0f, 3.0f, 1.0f, 1.0f, 2.0f, 0.0f, 0.0f, 0.0f );
+			.addBox( 3.0f, 8.0f, 1.0f+2.0f, 1.0f, 2.0f, 1.0f, 0.0f, 0.0f, 0.0f );
+		this.bipedLeftLeg.setTextureOffset( 26, 0 )
+			.addBox( 3.0f, 8.0f, 1.0f+3.0f, 1.0f, 1.0f, 2.0f, 0.0f, 0.0f, 0.0f );
 
 		this.bipedRightLeg.setTextureOffset( 16, 0 )
-			.addBox( -4.0f, 7.0f, -0.5f, 1.0f, 3.0f, 2.0f, 0.0f, 0.0f, 0.0f );
+			.addBox( -4.0f, 8.0f, 1.0f+0.0f, 1.0f, 3.0f, 2.0f, 0.0f, 0.0f, 0.0f );
 		this.bipedRightLeg.setTextureOffset( 22, 0 )
-			.addBox( -4.0f, 7.0f, 1.5f, 1.0f, 2.0f, 1.0f, 0.0f, 0.0f, 0.0f );
-		this.bipedRightLeg.setTextureOffset( 26, 10 )
-			.addBox( -4.0f, 7.0f, 2.5f, 1.0f, 1.0f, 2.0f, 0.0f, 0.0f, 0.0f );
+			.addBox( -4.0f, 8.0f, 1.0f+2.0f, 1.0f, 2.0f, 1.0f, 0.0f, 0.0f, 0.0f );
+		this.bipedRightLeg.setTextureOffset( 26, 0 )
+			.addBox( -4.0f, 8.0f, 1.0f+3.0f, 1.0f, 1.0f, 2.0f, 0.0f, 0.0f, 0.0f );
 	}
 }
