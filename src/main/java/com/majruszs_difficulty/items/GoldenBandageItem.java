0adac60ad9287a8b8b4b7a9cb69104d773636c4a
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-05-27 10:08:01 +0200

    Removed config for Bleeding immunity amplifier

--- src/main/java/com/majruszs_difficulty/items/GoldenBandageItem.java
+++ src/main/java/com/majruszs_difficulty/items/GoldenBandageItem.java
@@ -2,7 +2,6 @@ package com.majruszs_difficulty.items;
 
 import com.majruszs_difficulty.Instances;
 import com.mlib.config.DurationConfig;
-import com.mlib.config.IntegerConfig;
 import com.mlib.effects.EffectHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Rarity;
@@ -12,32 +11,24 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class GoldenBandageItem extends BandageItem {
 	protected final DurationConfig immunityDuration;
-	protected final IntegerConfig immunityAmplifier;
 
 	public GoldenBandageItem() {
 		super( "Golden Bandage", 1, Rarity.RARE );
 
 		String durationComment = "Duration in seconds of Bleeding Immunity effect.";
-		String amplifierComment = "Level/amplifier of Bleeding Immunity effect.";
 		this.immunityDuration = new DurationConfig( "immunity_duration", durationComment, false, 90.0, 1.0, 600.0 );
-		this.immunityAmplifier = new IntegerConfig( "immunity_amplifier", amplifierComment, false, 0, 0, 10 );
-		this.configGroup.addConfigs( this.immunityDuration, this.immunityAmplifier );
-	}
-
-	/** Returns duration in ticks of Bleeding immunity effect. */
-	public int getImmunityDuration() {
-		return this.immunityDuration.getDuration();
-	}
-
-	/** Returns amplifier of Bleeding immunity effect. */
-	public int getImmunityAmplifier() {
-		return this.immunityAmplifier.get();
+		this.configGroup.addConfigs( this.immunityDuration );
 	}
 
 	/** Applies Regeneration and Bleeding immunity on target depending on current mod settings. */
 	@Override
 	protected void applyEffects( LivingEntity target ) {
 		super.applyEffects( target );
-		EffectHelper.applyEffectIfPossible( target, Instances.BLEEDING_IMMUNITY, getImmunityDuration(), getImmunityAmplifier() );
+		EffectHelper.applyEffectIfPossible( target, Instances.BLEEDING_IMMUNITY, getImmunityDuration(), 0 );
+	}
+
+	/** Returns duration in ticks of Bleeding immunity effect. */
+	public int getImmunityDuration() {
+		return this.immunityDuration.getDuration();
 	}
 }
