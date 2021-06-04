f1292f2d0ed2264b50edabd56d23ea098c724363
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-06-04 15:52:12 +0200

    Refactoring

--- src/main/java/com/majruszs_difficulty/events/undead_army/Direction.java
+++ src/main/java/com/majruszs_difficulty/events/undead_army/Direction.java
@@ -11,29 +11,18 @@ import net.minecraft.world.server.ServerWorld;
 public enum Direction {
 	WEST( -1, 0 ), EAST( 1, 0 ), NORTH( 0, -1 ), SOUTH( 0, 1 );
 
-	private static final int STANDARD_DEVIATION = 10;
+	private static final int DISTANCE_MULTIPLIER = 10;
 	public final int x, z;
 	private final int xFactor, zFactor;
 
 	Direction( int x, int z ) {
 		this.x = x;
 		this.z = z;
-		this.xFactor = ( this.z != 0 ? 5 : 1 ) * STANDARD_DEVIATION;
-		this.zFactor = ( this.x != 0 ? 5 : 1 ) * STANDARD_DEVIATION;
+		this.xFactor = ( this.z != 0 ? 5 : 1 ) * DISTANCE_MULTIPLIER;
+		this.zFactor = ( this.x != 0 ? 5 : 1 ) * DISTANCE_MULTIPLIER;
 	}
 
-	/** Random position for entity to spawn. */
-	public BlockPos getRandomSpawnPosition( ServerWorld world, BlockPos positionToAttack, int spawnRadius ) {
-		Vector3d offset = Random.getRandomVector3d( -this.xFactor, this.xFactor, 0.0, 0.0, -this.zFactor, this.zFactor );
-
-		int x = positionToAttack.getX() + this.x * spawnRadius + ( int )offset.x;
-		int z = positionToAttack.getZ() + this.z * spawnRadius + ( int )offset.z;
-		int y = world.getHeight( Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, x, z ) + 1;
-
-		return new BlockPos( x, y, z );
-	}
-
-	/** Random direction. */
+	/** Returns random direction. */
 	public static Direction getRandom() {
 		return Direction.values()[ MajruszLibrary.RANDOM.nextInt( Direction.values().length ) ];
 	}
@@ -45,5 +34,16 @@ public enum Direction {
 				return direction;
 
 		return WEST;
+	}
+
+	/** Returns random position for entity to spawn in given direction. */
+	public BlockPos getRandomSpawnPosition( ServerWorld world, BlockPos positionToAttack, int spawnRadius ) {
+		Vector3d offset = Random.getRandomVector3d( -this.xFactor, this.xFactor, 0.0, 0.0, -this.zFactor, this.zFactor );
+
+		int x = positionToAttack.getX() + this.x * spawnRadius + ( int )offset.x;
+		int z = positionToAttack.getZ() + this.z * spawnRadius + ( int )offset.z;
+		int y = world.getHeight( Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, x, z ) + 1;
+
+		return new BlockPos( x, y, z );
 	}
 }
