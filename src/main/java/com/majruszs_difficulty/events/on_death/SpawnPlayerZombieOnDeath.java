d52476ade5943024c57b0bdb1f5357b48b7f84ef
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-06-08 20:19:23 +0200

    Added config to special Zombie on player's death

--- src/main/java/com/majruszs_difficulty/events/on_death/SpawnPlayerZombieOnDeath.java
+++ src/main/java/com/majruszs_difficulty/events/on_death/SpawnPlayerZombieOnDeath.java
@@ -2,7 +2,8 @@ package com.majruszs_difficulty.events.on_death;
 
 import com.majruszs_difficulty.GameState;
 import com.majruszs_difficulty.Instances;
-import com.mlib.MajruszLibrary;
+import com.mlib.Random;
+import com.mlib.config.DoubleConfig;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -23,27 +24,38 @@ import javax.annotation.Nullable;
 public class SpawnPlayerZombieOnDeath extends OnDeathBase {
 	private static final String CONFIG_NAME = "PlayerZombie";
 	private static final String CONFIG_COMMENT = "Spawns special Zombie whenever player dies from Bleeding or Zombie.";
+	protected final DoubleConfig playerHeadChance;
+	protected final DoubleConfig playerHeadDropChance;
 
 	public SpawnPlayerZombieOnDeath() {
 		super( CONFIG_NAME, CONFIG_COMMENT, 1.0, GameState.State.EXPERT, false );
+
+		String headComment = "Chance for spawned Zombie to have a player's head.";
+		this.playerHeadChance = new DoubleConfig( "head_chance", headComment, false, 1.0, 0.0, 1.0 );
+
+		String chanceComment = "Chance for special Zombie to drop a player's head on death. (if it has one)";
+		this.playerHeadDropChance = new DoubleConfig( "head_drop_chance", chanceComment, false, 0.01, 0.0, 1.0 );
+
+		this.featureGroup.addConfigs( this.playerHeadChance, this.playerHeadDropChance );
 	}
 
 	/** Called when all requirements were met. */
 	@Override
 	public void onExecute( @Nullable LivingEntity attacker, LivingEntity target, LivingDeathEvent event ) {
-		MajruszLibrary.LOGGER.info( "OOF!" );
-
 		ServerWorld world = ( ServerWorld )target.world;
 		PlayerEntity player = ( PlayerEntity )target;
-		Entity entity = EntityType.ZOMBIE.spawn( world, null, null, target.getPosition(), SpawnReason.EVENT, true, true );
+		Entity entity = EntityType.ZOMBIE.spawn( world, null, null, player.getPosition(), SpawnReason.EVENT, true, true );
 		if( entity == null )
 			return;
 
-		ItemStack playerSkull = getHead( player );
-
 		ZombieEntity zombie = ( ZombieEntity )entity;
-		zombie.setItemStackToSlot( EquipmentSlotType.HEAD, playerSkull );
-		zombie.setDropChance( EquipmentSlotType.HEAD, 0.0f );
+
+		if( Random.tryChance( this.playerHeadChance.get() ) ) {
+			ItemStack playerSkull = getHead( player );
+			zombie.setItemStackToSlot( EquipmentSlotType.HEAD, playerSkull );
+			zombie.setDropChance( EquipmentSlotType.HEAD, ( float )( double )this.playerHeadDropChance.get() );
+		}
+
 		zombie.setCustomName( player.getName() );
 		zombie.setCanPickUpLoot( true );
 		zombie.enablePersistence();
@@ -59,7 +71,7 @@ public class SpawnPlayerZombieOnDeath extends OnDeathBase {
 	}
 
 	/** Returns player's head item stack. */
-	private static ItemStack getHead( PlayerEntity player ) {
+	private ItemStack getHead( PlayerEntity player ) {
 		ItemStack playerSkull = new ItemStack( Items.PLAYER_HEAD, 1 );
 
 		CompoundNBT nbt = playerSkull.getOrCreateTag();
