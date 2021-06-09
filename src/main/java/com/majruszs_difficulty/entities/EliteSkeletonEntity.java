f87995049e90b9264ddf0313e2f2601e51fd65ca
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-06-09 17:11:20 +0200

    Decreased Elite Skeleton attack speed by 33% and added ability to shoot 3 arrows at once or perform a "quick shot"

--- src/main/java/com/majruszs_difficulty/entities/EliteSkeletonEntity.java
+++ src/main/java/com/majruszs_difficulty/entities/EliteSkeletonEntity.java
@@ -4,6 +4,7 @@ import com.majruszs_difficulty.GameState;
 import com.majruszs_difficulty.Instances;
 import com.majruszs_difficulty.MajruszsDifficulty;
 import com.mlib.MajruszLibrary;
+import com.mlib.Random;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -19,6 +20,7 @@ import net.minecraft.item.ArrowItem;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionUtils;
 import net.minecraft.potion.Potions;
@@ -32,7 +34,7 @@ import net.minecraft.world.server.ServerWorld;
 public class EliteSkeletonEntity extends SkeletonEntity {
 	public static final Potion[] arrowPotions = new Potion[]{ Potions.HARMING, Potions.POISON, Potions.SLOWNESS, Potions.WEAKNESS };
 	public static final EntityType< EliteSkeletonEntity > type;
-
+	private static final float ARROW_VELOCITY = 2.0f, ARROW_INACCURACY = 0.0f;
 	static {
 		type = EntityType.Builder.create( EliteSkeletonEntity::new, EntityClassification.MONSTER )
 			.size( 0.6f, 2.0f )
@@ -40,30 +42,37 @@ public class EliteSkeletonEntity extends SkeletonEntity {
 				.toString() );
 	}
 
-	public final RangedBowAttackGoal< AbstractSkeletonEntity > rangedAttackGoal;
+	private int quickShotsLeft = 0;
 
 	public EliteSkeletonEntity( EntityType< ? extends SkeletonEntity > type, World world ) {
 		super( type, world );
-		this.rangedAttackGoal = new RangedBowAttackGoal<>( this, 5.0 / 6.0, 15, 20.0f );
-		overwriteRangedAttackGoal();
+		this.aiArrowAttack = new RangedBowAttackGoal<>( this, 5.0 / 6.0, getAttackCooldown(), 20.0f );
 	}
 
 	/** Overriding basic Skeleton attack method with changes to inaccuracy, velocity and ammo this entity uses. */
 	@Override
 	public void attackEntityWithRangedAttack( LivingEntity target, float distanceFactor ) {
+		handleQuickShot();
+		if( this.quickShotsLeft == 0 && Random.tryChance( 0.25 ) )
+			shootExtraArrows( target, distanceFactor );
+
+		spawnArrow( 0.0, target, distanceFactor );
+		playSound( SoundEvents.ENTITY_SKELETON_SHOOT, 1.0f, 1.0f / ( MajruszLibrary.RANDOM.nextFloat() * 0.4f + 0.8f ) );
+	}
+
+	/** Spawns the arrow directed to target position. */
+	protected void spawnArrow( double yFactor, LivingEntity target, float distanceFactor ) {
 		ItemStack heldItemStack = getHeldItemMainhand();
 		AbstractArrowEntity arrowEntity = getArrowEntity( distanceFactor );
-
 		if( heldItemStack.getItem() instanceof BowItem )
 			arrowEntity = ( ( BowItem )heldItemStack.getItem() ).customArrow( arrowEntity );
 
 		double d0 = target.getPosX() - this.getPosX();
-		double d1 = target.getPosYHeight( 1.0 / 3.0 ) - arrowEntity.getPosY();
+		double d1 = target.getPosYHeight( 1.0 / 5.0 ) - arrowEntity.getPosY();
 		double d2 = target.getPosZ() - this.getPosZ();
 		double d3 = MathHelper.sqrt( d0 * d0 + d2 * d2 );
 
-		arrowEntity.shoot( d0, d1 + d3 * 0.2, d2, 2.0f, 0 ); // here is the change to velocity and inaccuracy
-		playSound( SoundEvents.ENTITY_SKELETON_SHOOT, 1.0f, 1.0f / ( MajruszLibrary.RANDOM.nextFloat() * 0.4f + 0.8f ) );
+		arrowEntity.shoot( d0, d1 + d3 * 0.2 + yFactor, d2, ARROW_VELOCITY, ARROW_INACCURACY );
 		this.world.addEntity( arrowEntity );
 	}
 
@@ -95,15 +104,26 @@ public class EliteSkeletonEntity extends SkeletonEntity {
 		return fireArrow( ammunition, distanceFactor );
 	}
 
-	/** Overwrites basic ranged attack goal with new one which gives all the bonuses to Skeleton. */
-	protected void overwriteRangedAttackGoal() {
-		ItemStack itemstack = getHeldItem( ProjectileHelper.getHandWith( this, Items.BOW ) );
+	/** Returns base cooldown between Elite Skeleton's attacks. */
+	protected int getAttackCooldown() {
+		return this.world.getDifficulty() != Difficulty.HARD ? 40 : 20;
+	}
 
-		if( itemstack.getItem() instanceof BowItem ) {
-			int attackCooldown = ( this.world.getDifficulty() != Difficulty.HARD ) ? 30 : 15;
-
-			this.rangedAttackGoal.setAttackCooldown( attackCooldown );
-			this.goalSelector.addGoal( 4, this.rangedAttackGoal );
+	/** Handles the Skeleton's shooting without any cooldown. (only the time it takes to draw the bowstring) */
+	protected void handleQuickShot() {
+		if( this.quickShotsLeft == 0 && Random.tryChance( 0.5 ) && this.world instanceof ServerWorld ) {
+			ServerWorld serverWorld = ( ServerWorld )this.world;
+			serverWorld.spawnParticle( ParticleTypes.ANGRY_VILLAGER, getPosX(), getPosYHeight( 0.5 ), getPosZ(), 6, 0.625, 0.25, 0.625, 0.125 );
+			this.quickShotsLeft = 3;
+			this.aiArrowAttack.setAttackCooldown( 1 );
+		} else if( this.quickShotsLeft > 0 && --this.quickShotsLeft == 0 ) {
+			this.aiArrowAttack.setAttackCooldown( getAttackCooldown() );
 		}
+	}
+
+	/** Shoots 2 extra arrows. */
+	protected void shootExtraArrows( LivingEntity target, float distanceFactor ) {
+		spawnArrow( 0.75, target, distanceFactor );
+		spawnArrow( -0.75, target, distanceFactor );
 	}
 }
