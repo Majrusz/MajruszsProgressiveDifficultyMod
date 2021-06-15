b50b6f1516ff19d10cc3a82faf95730c1510cf4e
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-06-15 17:56:51 +0200

    Updated version name and Majrusz Library

--- src/main/java/com/majruszs_difficulty/MajruszsDifficulty.java
+++ src/main/java/com/majruszs_difficulty/MajruszsDifficulty.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.config.ModConfig;
 public class MajruszsDifficulty {
 	public static final String MOD_ID = "majruszs_difficulty";
 	public static final String NAME = "Majrusz's Progressive Difficulty";
-	public static final String VERSION = "1.0.0-unofficial";
+	public static final String VERSION = "1.0.0";
 	public static final ConfigHandler CONFIG_HANDLER = new ConfigHandler( ModConfig.Type.COMMON, "majruszs-difficulty-common.toml" );
 	public static final ConfigGroup FEATURES_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Features", "" ) );
 	public static final ConfigGroup ENTITIES_GROUP = CONFIG_HANDLER.addConfigGroup( new ConfigGroup( "Entities", "" ) );
