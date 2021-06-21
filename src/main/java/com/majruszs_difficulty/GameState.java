d6f01c8ed4768eb1db821de9e71214f8acdd8eb5
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-06-21 15:14:27 +0200

    Update GameState.java

--- src/main/java/com/majruszs_difficulty/GameState.java
+++ src/main/java/com/majruszs_difficulty/GameState.java
@@ -2,7 +2,6 @@ package com.majruszs_difficulty;
 
 import com.mlib.WorldHelper;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
