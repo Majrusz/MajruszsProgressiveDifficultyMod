42826d9d3f964dd0c51758465c84f9c6fe6f2191
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-07-04 17:17:17 +0200

    Created separate function for teleporting entity nearby

--- src/main/java/com/majruszs_difficulty/MajruszsHelper.java
+++ src/main/java/com/majruszs_difficulty/MajruszsHelper.java
@@ -1,17 +1,22 @@
 package com.majruszs_difficulty;
 
+import com.mlib.Random;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.entity.monster.PillagerEntity;
 import net.minecraft.entity.monster.WitchEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.server.ServerWorld;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -66,5 +71,18 @@ public class MajruszsHelper {
 	public static void addExtraTooltipIfDisabled( List< ITextComponent > toolTip, boolean isEnabled ) {
 		if( !isEnabled )
 			toolTip.add( getDisabledItemTooltip() );
+	}
+
+	/** Teleports the target somewhere nearby. */
+	public static boolean teleportNearby( LivingEntity target, ServerWorld world, double insideOffset, double outsideOffset ) {
+		double distanceFactor = insideOffset;
+		if( target.prevPosY + 8 > world.getHeight( Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, ( int )target.prevPosX, ( int )target.prevPosZ ) )
+			distanceFactor = outsideOffset;
+
+		Vector3d newPosition = Random.getRandomVector3d( -distanceFactor, distanceFactor, -1.0, 1.0, -distanceFactor, distanceFactor )
+			.add( target.getPositionVec() );
+		double y = world.getHeight( Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, ( int )newPosition.x, ( int )newPosition.z ) + 1;
+
+		return !( y < 5 ) && target.attemptTeleport( newPosition.x, target.prevPosY + 8 > y ? y : newPosition.y, newPosition.z, true );
 	}
 }
--- src/main/java/com/majruszs_difficulty/features/when_damaged/EndermanTeleportOnAttack.java
+++ src/main/java/com/majruszs_difficulty/features/when_damaged/EndermanTeleportOnAttack.java
@@ -2,7 +2,7 @@ package com.majruszs_difficulty.features.when_damaged;
 
 import com.majruszs_difficulty.GameState;
 import com.majruszs_difficulty.Instances;
-import com.mlib.Random;
+import com.majruszs_difficulty.MajruszsHelper;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.monster.EndermanEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -10,8 +10,6 @@ import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
@@ -32,22 +30,14 @@ public class EndermanTeleportOnAttack extends ChanceWhenDamagedBase {
 			return;
 
 		ServerWorld world = ( ServerWorld )target.world;
-
-		double distanceFactor = 5.0;
-		if( target.prevPosY + 8 > world.getHeight( Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, ( int )target.prevPosX, ( int )target.prevPosZ ) )
-			distanceFactor *= 2.0;
-
-		Vector3d newPosition = Random.getRandomVector3d( -distanceFactor, distanceFactor, -1.0, 1.0, -distanceFactor, distanceFactor )
-			.add( target.getPositionVec() );
-		double y = world.getHeight( Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, ( int )newPosition.x, ( int )newPosition.z ) + 1;
-
-		if( y < 5 )
+		if( !MajruszsHelper.teleportNearby( target, world, 5.0, 10.0 ) )
 			return;
 
-		world.playSound( null, target.prevPosX, target.prevPosY, target.prevPosZ, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1.0f, 1.0f );
+		world.playSound( null, target.prevPosX, target.prevPosY, target.prevPosZ, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1.0f,
+			1.0f
+		);
 		world.spawnParticle( ParticleTypes.PORTAL, target.prevPosX, target.getPosYHeight( 0.5 ), target.prevPosZ, 10, 0.25, 0.25, 0.25, 0.1 );
-		boolean teleportedTarget = target.attemptTeleport( newPosition.x, target.prevPosY + 8 > y ? y : newPosition.y, newPosition.z, true );
-		if( teleportedTarget && target instanceof ServerPlayerEntity )
+		if( target instanceof ServerPlayerEntity )
 			Instances.SIMPLE_TRIGGER.trigger( ( ServerPlayerEntity )target, "enderman_teleport_attack" );
 	}
 
