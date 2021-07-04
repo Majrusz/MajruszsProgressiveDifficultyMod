1693dcc145a89d67e5e4e00ac5377b4086534863
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-07-04 16:09:52 +0200

    Added configs to Infested effect

--- src/main/java/com/majruszs_difficulty/effects/InfestedEffect.java
+++ src/main/java/com/majruszs_difficulty/effects/InfestedEffect.java
@@ -3,14 +3,18 @@ package com.majruszs_difficulty.effects;
 import com.majruszs_difficulty.Instances;
 import com.majruszs_difficulty.entities.ParasiteEntity;
 import com.mlib.Random;
+import com.mlib.config.ConfigGroup;
+import com.mlib.config.DoubleConfig;
+import com.mlib.config.DurationConfig;
+import com.mlib.effects.EffectHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.item.ItemStack;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.EffectType;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.server.ServerWorld;
@@ -21,12 +25,28 @@ import net.minecraftforge.fml.common.Mod;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.majruszs_difficulty.MajruszsDifficulty.FEATURES_GROUP;
+
 /** Effect that spawns parasites after it expires. */
 @Mod.EventBusSubscriber
 public class InfestedEffect extends Effect {
+	protected final ConfigGroup effectGroup;
+	protected final DurationConfig duration;
+	protected final DoubleConfig damage;
+
 	public InfestedEffect() {
 		super( EffectType.HARMFUL, 0xff161616 );
+
+		String durationComment = "Duration of this effect whenever it is applied by Parasite. (in seconds)";
+		this.duration = new DurationConfig( "duration", durationComment, false, 15.0, 1.0, 120.0 );
+
+		String damageComment = "Damage dealt after this effect expires. (per level) (1.0 = half a heart)";
+		this.damage = new DurationConfig( "damage", damageComment, false, 1.0, 0.0, 20.0 );
+
+		this.effectGroup = FEATURES_GROUP.addGroup( new ConfigGroup( "Infested", "Infested potion effect." ) );
+		this.effectGroup.addConfigs( this.duration, this.damage );
 	}
+
 	/** Removes default milk bucket from curative items. */
 	@Override
 	public List< ItemStack > getCurativeItems() {
@@ -38,12 +58,27 @@ public class InfestedEffect extends Effect {
 	public static void onEffectExpired( PotionEvent.PotionExpiryEvent event ) {
 		EffectInstance effectInstance = event.getPotionEffect();
 		LivingEntity target = event.getEntityLiving();
-		if( effectInstance == null || !Instances.INFESTED.equals( effectInstance.getPotion() ) || !( target.world instanceof ServerWorld ) )
+		InfestedEffect infestedEffect = Instances.INFESTED;
+		if( effectInstance == null || !infestedEffect.equals( effectInstance.getPotion() ) || !( target.world instanceof ServerWorld ) )
 			return;
 
+		infestedEffect.spawnParasites( effectInstance, target, ( ServerWorld )target.world );
+		infestedEffect.damageEntity( effectInstance, target );
+	}
+
+	/** Applies Infested on given target or if it has one then it increases the amplifier. */
+	public void applyTo( LivingEntity target ) {
+		EffectInstance currentEffect = target.getActivePotionEffect( this );
+		int amplifier = currentEffect != null ? currentEffect.getAmplifier() + 1 : 0;
+
+		EffectHelper.applyEffectIfPossible( target, this, this.duration.getDuration(), amplifier );
+	}
+
+	/** Spawns parasites depending on potion amplifier and near the target. */
+	protected void spawnParasites( EffectInstance effectInstance, LivingEntity target, ServerWorld world ) {
+		int effectAmplifier = effectInstance.getAmplifier();
 		BlockPos targetPosition = target.getPosition();
-		ServerWorld world = ( ServerWorld )target.world;
-		for( int i = 0; i < 2; ++i ) {
+		for( int i = 0; i < effectAmplifier + 2; ++i ) {
 			Vector3d offset = Random.getRandomVector3d( -1.0, 1.0, 0.0, 0.0, -1.0, 1.0 );
 			BlockPos parasitePosition = targetPosition.add( offset.x, 0.0, offset.z );
 			Entity entity = ParasiteEntity.type.spawn( world, null, null, parasitePosition, SpawnReason.EVENT, true, true );
@@ -54,5 +89,10 @@ public class InfestedEffect extends Effect {
 			parasite.setAttackTarget( target );
 			ParasiteEntity.spawnEffects( world, parasitePosition );
 		}
+	}
+
+	/** Damages the target depending on potion amplifier. */
+	protected void damageEntity( EffectInstance effectInstance, LivingEntity target ) {
+		target.attackEntityFrom( DamageSource.MAGIC, ( float )( ( effectInstance.getAmplifier() + 1 ) * this.damage.get() ) );
 	}
 }
