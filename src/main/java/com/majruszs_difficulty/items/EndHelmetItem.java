5bae647b62e061d9dc93fe459e10c574fdc09145
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-07-15 19:34:01 +0200

    End Helmet now also makes endermans not target you when you look at them

--- src/main/java/com/majruszs_difficulty/items/EndHelmetItem.java
+++ src/main/java/com/majruszs_difficulty/items/EndHelmetItem.java
@@ -1,10 +1,19 @@
 package com.majruszs_difficulty.items;
 
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ItemStack;
 
 /** New late game helmet. */
 public class EndHelmetItem extends EndArmorItem {
 	public EndHelmetItem() {
 		super( EquipmentSlotType.HEAD );
+	}
+
+	@Override
+	public boolean isEnderMask( ItemStack stack, PlayerEntity player, EndermanEntity endermanEntity ) {
+		return true;
 	}
 }
