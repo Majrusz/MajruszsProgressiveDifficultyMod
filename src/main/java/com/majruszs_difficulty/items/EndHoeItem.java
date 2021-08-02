742d40c9f0cf2c392368e3770fe5c56081cb5a5c
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-08-02 18:28:47 +0200

    Removed temporary End Hoe till ability

--- src/main/java/com/majruszs_difficulty/items/EndHoeItem.java
+++ src/main/java/com/majruszs_difficulty/items/EndHoeItem.java
@@ -3,6 +3,7 @@ package com.majruszs_difficulty.items;
 import com.majruszs_difficulty.Instances;
 import com.majruszs_difficulty.MajruszsHelper;
 import com.majruszs_difficulty.features.end_items.EndItems;
+import com.mlib.MajruszLibrary;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
@@ -34,8 +35,8 @@ public class EndHoeItem extends HoeItem {
 		Player player = context.getPlayer();
 		InteractionResult resultType = super.useOn( context );
 
-		if( player != null && !player.isCrouching() && resultType == InteractionResult.CONSUME )
-			tillNearbyBlocks( context );
+		// if( player != null && !player.isCrouching() && resultType == InteractionResult.CONSUME )
+		// tillNearbyBlocks( context );
 
 		return resultType;
 	}
@@ -44,9 +45,8 @@ public class EndHoeItem extends HoeItem {
 	@OnlyIn( Dist.CLIENT )
 	public void appendHoverText( ItemStack itemStack, @Nullable Level world, List< Component > tooltip, TooltipFlag flag ) {
 		MajruszsHelper.addExtraTooltipIfDisabled( tooltip, Instances.END_SHARD_ORE.isEnabled() );
-		MajruszsHelper.addAdvancedTooltips( tooltip, flag, EndItems.Keys.BLEED_TOOLTIP, EndItems.Keys.HASTE_TOOLTIP, EndItems.Keys.LEVITATION_TOOLTIP,
-			EndItems.Keys.TILL_TOOLTIP
-		);
+		MajruszsHelper.addAdvancedTooltips( tooltip, flag, EndItems.Keys.BLEED_TOOLTIP, EndItems.Keys.HASTE_TOOLTIP, EndItems.Keys.LEVITATION_TOOLTIP/*,
+			EndItems.Keys.TILL_TOOLTIP*/ );
 	}
 
 	/** Tills nearby blocks in 3x1x3 grid around the context. */
@@ -55,21 +55,29 @@ public class EndHoeItem extends HoeItem {
 		BlockPos blockPosition = context.getClickedPos();
 		Player player = context.getPlayer();
 		ItemStack itemStack = context.getItemInHand();
+		// Pair< Predicate< UseOnContext >, Consumer< UseOnContext > > pair = TILLABLES.get( world.getBlockState( blockPosition ).getBlock() );
+		// Predicate< UseOnContext > predicate = pair.getFirst();
+		// Consumer< UseOnContext > consumer = pair.getSecond();
 
 		for( int x = -1; x <= 1; ++x )
 			for( int z = -1; z <= 1; ++z ) {
 				if( x == 0 && z == 0 )
 					continue;
-
+				MajruszLibrary.LOGGER.info( x + "," + z );
 				BlockPos newPosition = blockPosition.offset( x, 0, z );
 				if( !world.isEmptyBlock( newPosition.above() ) )
 					continue;
 
+				MajruszLibrary.LOGGER.info( newPosition );
+				MajruszLibrary.LOGGER.info( "isEmpty " + world.getBlockState( newPosition ) + " " + world.getBlockState( newPosition )
+					.getToolModifiedState( world, newPosition, player, itemStack, ToolType.HOE ) );
 				BlockState blockState = world.getBlockState( newPosition )
 					.getToolModifiedState( world, newPosition, player, itemStack, ToolType.HOE );
+				MajruszLibrary.LOGGER.info( ( blockState == null ) + " " + ( world.isClientSide ) );
 				if( blockState == null || world.isClientSide )
 					continue;
 
+				MajruszLibrary.LOGGER.info( "set" );
 				world.setBlock( newPosition, blockState, 11 );
 				if( player != null )
 					itemStack.hurtAndBreak( 1, player, user->user.broadcastBreakEvent( context.getHand() ) );
