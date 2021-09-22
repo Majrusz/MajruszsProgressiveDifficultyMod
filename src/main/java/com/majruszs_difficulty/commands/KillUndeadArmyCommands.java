d2c72167d47095a961241b90af3e4856f802c519
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-22 19:25:08 +0200

    Added new killall command

---
+++ src/main/java/com/majruszs_difficulty/commands/KillUndeadArmyCommands.java
@@ -0,0 +1,35 @@
+package com.majruszs_difficulty.commands;
+
+import com.majruszs_difficulty.RegistryHandler;
+import com.majruszs_difficulty.features.undead_army.UndeadArmy;
+import com.mlib.commands.IRegistrableCommand;
+import com.mlib.commands.PositionCommand;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.phys.Vec3;
+
+/** Command that kills all mobs from the Undead Army at given position. */
+public class KillUndeadArmyCommands extends PositionCommand implements IRegistrableCommand {
+	/** Kills all mobs from the Undead Army at given position and sends information to the caller. */
+	@Override
+	protected int handleCommand( CommandContext< CommandSourceStack > context, CommandSourceStack source, Vec3 position ) {
+		UndeadArmy undeadArmy = RegistryHandler.UNDEAD_ARMY_MANAGER.findNearestUndeadArmy( new BlockPos( position ) );
+		if( undeadArmy != null ) {
+			undeadArmy.killAllUndeadArmyEntities();
+			source.sendSuccess( CommandsHelper.createBaseMessageWithPosition( "commands.undeadarmy.killed", position ), true );
+			return 0;
+		}
+
+		source.sendSuccess( CommandsHelper.createBaseMessageWithPosition( "commands.undeadarmy.missing", position ), true );
+		return -1;
+	}
+
+	/** Registers this command. */
+	@Override
+	public void register( CommandDispatcher< CommandSourceStack > commandDispatcher ) {
+		Data commandData = new Data( hasPermission( 4 ), CommandsHelper.UNDEAD_ARMY_ARGUMENT, literal( "killall" ) );
+		registerLocationCommand( commandDispatcher, commandData );
+	}
+}
