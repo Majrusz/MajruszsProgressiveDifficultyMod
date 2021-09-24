833e02303476b5061d0cd7a0788ccf7022e72a94
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-24 21:45:01 +0200

    Increased color and size variation for bleeding effect

--- src/main/java/com/majruszs_difficulty/particles/BloodParticle.java
+++ src/main/java/com/majruszs_difficulty/particles/BloodParticle.java
@@ -1,9 +1,15 @@
 package com.majruszs_difficulty.particles;
 
 import com.mlib.MajruszLibrary;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.particle.*;
 import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
@@ -14,7 +20,7 @@ public class BloodParticle extends TextureSheetParticle {
 		this.xd = this.xd * 0.0025 + xSpeed;
 		this.yd = this.yd * 0.02 + ySpeed;
 		this.zd = this.zd * 0.0025 + zSpeed;
-		this.quadSize *= 0.25 + MajruszLibrary.RANDOM.nextDouble() * 0.25;
+		this.quadSize *= 0.45 + MajruszLibrary.RANDOM.nextDouble() * 0.25;
 
 		this.lifetime = ( 20 * 10 + MajruszLibrary.RANDOM.nextInt( 60 ) );
 	}
@@ -47,6 +53,44 @@ public class BloodParticle extends TextureSheetParticle {
 	}
 
 	@Override
+	public void render( VertexConsumer vertexConsumer, Camera camera, float scaleFactor ) {
+		Vec3 vec3 = camera.getPosition();
+		float f = (float)( Mth.lerp((double)scaleFactor , this.xo, this.x) - vec3.x());
+		float f1 = (float)(Mth.lerp((double)scaleFactor , this.yo, this.y) - vec3.y());
+		float f2 = (float)(Mth.lerp((double)scaleFactor , this.zo, this.z) - vec3.z());
+		Quaternion quaternion;
+		if (this.roll == 0.0F) {
+			quaternion = camera.rotation();
+		} else {
+			quaternion = new Quaternion(camera.rotation());
+			float f3 = Mth.lerp(scaleFactor , this.oRoll, this.roll);
+			quaternion.mul( Vector3f.ZP.rotation(f3));
+		}
+
+		Vector3f vector3f1 = new Vector3f(-1.0F, -1.0F, 0.0F);
+		vector3f1.transform(quaternion);
+		Vector3f[] avector3f = new Vector3f[]{new Vector3f(-1.0F, -1.0F, 0.0F), new Vector3f(-1.0F, 1.0F, 0.0F), new Vector3f(1.0F, 1.0F, 0.0F), new Vector3f(1.0F, -1.0F, 0.0F)};
+		float f4 = this.getQuadSize(scaleFactor );
+
+		for(int i = 0; i < 4; ++i) {
+			Vector3f vector3f = avector3f[i];
+			vector3f.transform(quaternion);
+			vector3f.mul(f4);
+			vector3f.add(f, f1, f2);
+		}
+
+		float f7 = this.getU0();
+		float f8 = this.getU1();
+		float f5 = this.getV0();
+		float f6 = this.getV1();
+		int j = this.getLightColor(scaleFactor );
+		vertexConsumer.vertex((double)avector3f[0].x(), (double)avector3f[0].y(), (double)avector3f[0].z()).uv(f8, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
+		vertexConsumer.vertex((double)avector3f[1].x(), (double)avector3f[1].y(), (double)avector3f[1].z()).uv(f8, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
+		vertexConsumer.vertex((double)avector3f[2].x(), (double)avector3f[2].y(), (double)avector3f[2].z()).uv(f7, f5).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
+		vertexConsumer.vertex((double)avector3f[3].x(), (double)avector3f[3].y(), (double)avector3f[3].z()).uv(f7, f6).color(this.rCol, this.gCol, this.bCol, this.alpha).uv2(j).endVertex();
+	}
+	
+	@Override
 	public ParticleRenderType getRenderType() {
 		return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
 	}
@@ -65,7 +109,7 @@ public class BloodParticle extends TextureSheetParticle {
 		) {
 			BloodParticle particle = new BloodParticle( world, x, y, z, xSpeed, ySpeed, zSpeed );
 			particle.pickSprite( this.spriteSet );
-			particle.setColor( 1.0f, 0.2f, 0.2f );
+			particle.setColor( 1.0f, MajruszLibrary.RANDOM.nextFloat() * 0.2f + 0.1f, MajruszLibrary.RANDOM.nextFloat() * 0.2f + 0.1f );
 
 			return particle;
 		}
