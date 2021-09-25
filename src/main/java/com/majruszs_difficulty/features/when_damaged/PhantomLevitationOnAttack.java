5c881af2583ee89740821d92a61155782af6f037
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-25 15:50:25 +0200

    Phantom now has a chance to inflict Levitation

--- src/main/java/com/majruszs_difficulty/features/when_damaged/PhantomLevitationOnAttack.java
+++ src/main/java/com/majruszs_difficulty/features/when_damaged/PhantomLevitationOnAttack.java
@@ -1,22 +1,22 @@
 package com.majruszs_difficulty.features.when_damaged;
 
 import com.majruszs_difficulty.GameState;
-import com.majruszs_difficulty.entities.SkyKeeperEntity;
 import com.mlib.TimeConverter;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Phantom;
 
 import javax.annotation.Nullable;
 
-/** Making Sky Keeper attacks have a chance to set levitation on enemies. */
-public class SkyKeeperLevitationOnAttack extends WhenDamagedApplyStackableEffectBase {
-	private static final String CONFIG_NAME = "SkyKeeperLevitation";
-	private static final String CONFIG_COMMENT = "Sky Keeper inflicts levitation.";
+/** Makes Phantom attacks have a chance to set levitation on enemies. */
+public class PhantomLevitationOnAttack extends WhenDamagedApplyStackableEffectBase {
+	private static final String CONFIG_NAME = "PhantomLevitation";
+	private static final String CONFIG_COMMENT = "Phantom inflicts levitation.";
 
-	public SkyKeeperLevitationOnAttack() {
-		super( CONFIG_NAME, CONFIG_COMMENT, 0.5, 6.0, GameState.State.NORMAL, true, MobEffects.LEVITATION, false, true, 1,
+	public PhantomLevitationOnAttack() {
+		super( CONFIG_NAME, CONFIG_COMMENT, 0.5, 6.0, GameState.State.MASTER, true, MobEffects.LEVITATION, false, true, 1,
 			TimeConverter.secondsToTicks( 60.0 )
 		);
 	}
@@ -24,7 +24,7 @@ public class SkyKeeperLevitationOnAttack extends WhenDamagedApplyStackableEffect
 	/** Checking if all conditions were met. */
 	@Override
 	public boolean shouldBeExecuted( @Nullable LivingEntity attacker, LivingEntity target, DamageSource damageSource ) {
-		return attacker instanceof SkyKeeperEntity && super.shouldBeExecuted( attacker, target, damageSource );
+		return attacker instanceof Phantom && super.shouldBeExecuted( attacker, target, damageSource );
 	}
 
 	@Override
