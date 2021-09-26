8a9397f2967c27340c97af8ba0f6ff5e8e46d4fd
Majrusz <mateuszsmolarkiewicz@wp.pl>
2021-09-26 11:05:23 +0200

    Tank now has basic AI

--- src/main/java/com/majruszs_difficulty/entities/TankEntity.java
+++ src/main/java/com/majruszs_difficulty/entities/TankEntity.java
@@ -1,11 +1,16 @@
 package com.majruszs_difficulty.entities;
 
 import com.majruszs_difficulty.MajruszsDifficulty;
+import com.majruszs_difficulty.goals.TankAttackGoal;
 import com.mlib.MajruszLibrary;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.*;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 
@@ -15,7 +20,7 @@ public class TankEntity extends Monster {
 
 	static {
 		type = EntityType.Builder.of( TankEntity::new, MobCategory.MONSTER )
-			.sized( 1.2f, 4.0f )
+			.sized( 1.1f, 2.7f )
 			.build( MajruszsDifficulty.getLocation( "tank" ).toString() );
 	}
 
@@ -24,12 +29,15 @@ public class TankEntity extends Monster {
 		this.xpReward = 17;
 	}
 
-	/** Registration of the entity's basic goals. */
 	@Override
 	protected void registerGoals() {
-		/*this.goalSelector.addGoal( 8, new LookAtPlayerGoal( this, Player.class, 8.0f ) );
-		this.goalSelector.addGoal( 8, new RandomLookAroundGoal( this ) );*/
-		this.applyEntityAI();
+		this.goalSelector.addGoal(2, new TankAttackGoal( this ) );
+		this.goalSelector.addGoal( 7, new WaterAvoidingRandomStrollGoal( this, 1.0 ) );
+		this.goalSelector.addGoal( 8, new LookAtPlayerGoal( this, Player.class, 8.0f ) );
+		this.goalSelector.addGoal( 8, new RandomLookAroundGoal( this ) );
+
+		this.targetSelector.addGoal( 2, new NearestAttackableTargetGoal<>( this, Player.class, true ) );
+		this.targetSelector.addGoal( 3, new NearestAttackableTargetGoal<>( this, IronGolem.class, true ) );
 	}
 
 	@Override
@@ -39,7 +47,7 @@ public class TankEntity extends Monster {
 
 	@Override
 	protected float getStandingEyeHeight( Pose poseIn, EntityDimensions sizeIn ) {
-		return 3.6f;
+		return 2.35f;
 	}
 
 	/*@Override
@@ -52,21 +60,10 @@ public class TankEntity extends Monster {
 		return Mob.createMobAttributes()
 			.add( Attributes.MAX_HEALTH, 100.0 )
 			.add( Attributes.MOVEMENT_SPEED, 0.25 )
-			.add( Attributes.ATTACK_DAMAGE, 10.0 )
+			.add( Attributes.ATTACK_DAMAGE, 8.0 )
 			.add( Attributes.FOLLOW_RANGE, 40.0 )
 			.add( Attributes.ATTACK_KNOCKBACK, 2.0 )
 			.add( Attributes.KNOCKBACK_RESISTANCE, 0.5 )
 			.build();
-	}
-
-	protected void applyEntityAI() {
-		/*this.goalSelector.addGoal( 2, new GiantAttackGoal( this, 1.0, false ) );
-		this.goalSelector.addGoal( 6, new MoveThroughVillageGoal( this, 1.0, true, 4, this::canBreakDoors ) );
-		this.goalSelector.addGoal( 7, new WaterAvoidingRandomStrollGoal( this, 1.0 ) );
-		this.targetSelector.addGoal( 1, ( new HurtByTargetGoal( this ) ).setAlertOthers( ZombifiedPiglin.class ) );
-		this.targetSelector.addGoal( 2, new NearestAttackableTargetGoal<>( this, Player.class, true ) );
-		this.targetSelector.addGoal( 3, new NearestAttackableTargetGoal<>( this, AbstractVillager.class, false ) );
-		this.targetSelector.addGoal( 3, new NearestAttackableTargetGoal<>( this, IronGolem.class, true ) );
-		this.targetSelector.addGoal( 5, new NearestAttackableTargetGoal<>( this, Turtle.class, 10, true, false, Turtle.BABY_ON_LAND_SELECTOR ) );*/
 	}
 }
