b46cd9438c22f6ab150155b125b0898542756330
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-06-20 18:03:27 +0200

    Changed Enderium Sword damage 9 -> 10 and attack speed 1.6 -> 1.4

--- src/main/java/com/majruszsdifficulty/items/EnderiumSwordItem.java
+++ src/main/java/com/majruszsdifficulty/items/EnderiumSwordItem.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 public class EnderiumSwordItem extends SwordItem {
 	public EnderiumSwordItem() {
-		super( CustomItemTier.END, 3, -2.4f, new Properties().tab( Registries.ITEM_GROUP ).rarity( Rarity.UNCOMMON ).fireResistant() );
+		super( CustomItemTier.END, 4, -2.6f, new Properties().tab( Registries.ITEM_GROUP ).rarity( Rarity.UNCOMMON ).fireResistant() );
 	}
 
 	@Override
