5c05dd82038e6a0d19ed2bbdb71310aa5e32f606
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-06-20 18:04:06 +0200

    Added Undead Army armor to creative tab

--- src/main/java/com/majruszsdifficulty/items/ClothItem.java
+++ src/main/java/com/majruszsdifficulty/items/ClothItem.java
@@ -1,11 +1,25 @@
 package com.majruszsdifficulty.items;
 
 import com.majruszsdifficulty.Registries;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
 /** Cloth required to make Undead Army Battle Standard. */
 public class ClothItem extends Item {
 	public ClothItem() {
 		super( new Properties().tab( Registries.ITEM_GROUP ) );
+	}
+
+	@Override
+	public void fillItemCategory( CreativeModeTab itemGroup, NonNullList< ItemStack > itemStacks ) {
+		if( itemGroup != Registries.ITEM_GROUP )
+			return;
+
+		itemStacks.add( UndeadArmorItem.constructItem( UndeadArmorItem.HELMET_ID ) );
+		itemStacks.add( UndeadArmorItem.constructItem( UndeadArmorItem.CHESTPLATE_ID ) );
+		itemStacks.add( UndeadArmorItem.constructItem( UndeadArmorItem.LEGGINGS_ID ) );
+		itemStacks.add( UndeadArmorItem.constructItem( UndeadArmorItem.BOOTS_ID ) );
 	}
 }
