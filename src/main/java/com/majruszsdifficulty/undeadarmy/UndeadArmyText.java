1817e0fe2214112ce8d222d851f6659f841315bb
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-06-27 16:07:17 +0200

    Refactor

--- src/main/java/com/majruszsdifficulty/undeadarmy/UndeadArmyText.java
+++ src/main/java/com/majruszsdifficulty/undeadarmy/UndeadArmyText.java
@@ -7,38 +7,24 @@ import net.minecraft.server.level.ServerPlayer;
 
 import java.util.List;
 
-/** Easier handling formatted text for Undead Army. */
 public class UndeadArmyText {
-	public static final Component TITLE = Component.translatable( "majruszsdifficulty.undead_army.title" );
-	public static final Component WAVE = Component.translatable( "majruszsdifficulty.undead_army.wave" );
-	public static final Component BETWEEN_WAVES = Component.translatable( "majruszsdifficulty.undead_army.between_waves" );
-	public static final Component VICTORY = Component.translatable( "majruszsdifficulty.undead_army.victory" );
-	public static final Component FAILED = Component.translatable( "majruszsdifficulty.undead_army.failed" );
-	public static final Component APPROACHING = Component.translatable( "majruszsdifficulty.undead_army.approaching" );
+	public static final MutableComponent TITLE = Component.translatable( "majruszsdifficulty.undead_army.title" );
+	public static final MutableComponent WAVE = Component.translatable( "majruszsdifficulty.undead_army.wave" );
+	public static final MutableComponent BETWEEN_WAVES = Component.translatable( "majruszsdifficulty.undead_army.between_waves" );
+	public static final MutableComponent VICTORY = Component.translatable( "majruszsdifficulty.undead_army.victory" );
+	public static final MutableComponent FAILED = Component.translatable( "majruszsdifficulty.undead_army.failed" );
+	public static final MutableComponent APPROACHING = Component.translatable( "majruszsdifficulty.undead_army.approaching" );
 
-	/** Returns formatted text with information about current wave. */
-	public static Component getWaveMessage( int currentWave ) {
-		MutableComponent message = Component.literal( "" );
-		message.append( TITLE );
-		message.append( " (" );
-		message.append( WAVE );
-		message.append( " " + currentWave + ")" );
-
-		return message;
+	public static MutableComponent constructWaveMessage( int currentWave ) {
+		return Component.literal( "" ).append( TITLE ).append( " (" ).append( WAVE ).append( " " + currentWave + ")" );
 	}
 
-	/** Sends information about Undead Army start to every given player. */
-	public static void notifyAboutStart( List< ServerPlayer > players, Direction direction ) {
-		String directionAsString = direction.toString();
-
-		MutableComponent message = APPROACHING.copy();
-		message.append( " " );
-		message.append( Component.translatable( "majruszsdifficulty.undead_army." + directionAsString.toLowerCase() ) );
-		message.append( "!" );
-		message.withStyle( ChatFormatting.BOLD );
-		message.withStyle( ChatFormatting.DARK_PURPLE );
-
-		for( ServerPlayer player : players )
-			player.displayClientMessage( message, false );
+	public static MutableComponent constructDirectionMessage( Direction direction ) {
+		return APPROACHING.copy()
+			.append( " " )
+			.append( Component.translatable( "majruszsdifficulty.undead_army." + direction.toString().toLowerCase() ) )
+			.append( "!" )
+			.withStyle( ChatFormatting.BOLD )
+			.withStyle( ChatFormatting.DARK_PURPLE );
 	}
 }
