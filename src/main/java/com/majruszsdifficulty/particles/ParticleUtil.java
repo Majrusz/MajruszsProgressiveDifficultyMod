e009b4290bc95c4b03fee48622ec2767bdf89fdd
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-07-09 10:17:44 +0200

    Fixed server crash

--- src/main/java/com/majruszsdifficulty/particles/ParticleUtil.java
+++ src/main/java/com/majruszsdifficulty/particles/ParticleUtil.java
@@ -2,12 +2,15 @@ package com.majruszsdifficulty.particles;
 
 import com.majruszsdifficulty.MajruszsDifficulty;
 import com.majruszsdifficulty.Registries;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber( modid = MajruszsDifficulty.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD )
 public class ParticleUtil {
+	@OnlyIn( Dist.CLIENT )
 	@SubscribeEvent
 	public static void registerParticles( RegisterParticleProvidersEvent event ) {
 		event.register( Registries.BLOOD.get(), BloodParticle.Factory::new );
