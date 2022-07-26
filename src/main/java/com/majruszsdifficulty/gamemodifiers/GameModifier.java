95778d81fa50df4655e2368cb6d84c2e922b9da8
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-07-26 19:12:26 +0200

    Fixed game modifier names

--- src/main/java/com/majruszsdifficulty/gamemodifiers/GameModifier.java
+++ src/main/java/com/majruszsdifficulty/gamemodifiers/GameModifier.java
@@ -1,13 +1,13 @@
 package com.majruszsdifficulty.gamemodifiers;
 
-import com.majruszsdifficulty.MajruszsDifficulty;
+import com.majruszsdifficulty.Registries;
 
 public abstract class GameModifier extends com.mlib.gamemodifiers.GameModifier {
-	public static final String DEFAULT = MajruszsDifficulty.MOD_ID + "Default";
-	public static final String UNDEAD_ARMY = MajruszsDifficulty.MOD_ID + "UndeadArmy";
-	public static final String GAME_STAGE = MajruszsDifficulty.MOD_ID + "GameStage";
-	public static final String TREASURE_BAG = MajruszsDifficulty.MOD_ID + "TreasureBag";
-	public static final String ACCESSORY = MajruszsDifficulty.MOD_ID + "Accessory";
+	public static final String DEFAULT = Registries.getLocationString( "default" );
+	public static final String UNDEAD_ARMY = Registries.getLocationString( "undead_army" );
+	public static final String GAME_STAGE = Registries.getLocationString( "game_stage" );
+	public static final String TREASURE_BAG = Registries.getLocationString( "treasure_bag" );
+	public static final String ACCESSORY = Registries.getLocationString( "accessory" );
 
 	public GameModifier( String configKey, String configName, String configComment ) {
 		super( configKey, configName, configComment );
