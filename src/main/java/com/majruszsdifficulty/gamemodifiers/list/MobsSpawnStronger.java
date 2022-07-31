eb515c4f2a73d172a96906ff8b5881ae9679e811
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-07-31 20:29:31 +0200

    Added config to disable all entities from specified dimensions from getting health and damage bonuses

--- src/main/java/com/majruszsdifficulty/gamemodifiers/list/MobsSpawnStronger.java
+++ src/main/java/com/majruszsdifficulty/gamemodifiers/list/MobsSpawnStronger.java
@@ -2,12 +2,13 @@ package com.majruszsdifficulty.gamemodifiers.list;
 
 import com.majruszsdifficulty.config.GameStageDoubleConfig;
 import com.majruszsdifficulty.gamemodifiers.DifficultyModifier;
+import com.mlib.Utility;
 import com.mlib.attributes.AttributeHandler;
 import com.mlib.config.StringListConfig;
 import com.mlib.gamemodifiers.Condition;
 import com.mlib.gamemodifiers.contexts.OnSpawnedContext;
 import com.mlib.gamemodifiers.data.OnSpawnedData;
-import net.minecraft.world.entity.EntityType;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -20,6 +21,7 @@ public class MobsSpawnStronger extends DifficultyModifier {
 	final GameStageDoubleConfig damageBonus = new GameStageDoubleConfig( "DamageBonusMultiplier", "", 0.0, 0.15, 0.3, 0.0, 10.0 );
 	final GameStageDoubleConfig nightMultiplier = new GameStageDoubleConfig( "NightMultiplier", "Multiplies health and damage bonuses at night.", 2.0, 2.0, 2.0, 1.0, 10.0 );
 	final StringListConfig excludedMobs = new StringListConfig( "excluded_mobs", "List of mobs that should not get health and damage bonuses. (for instance minecraft:wither)", false );
+	final StringListConfig excludedDimensions = new StringListConfig( "excluded_dimensions", "List of dimensions where health and damage bonuses should not be applied. (for instance minecraft:overworld)", false );
 
 	public MobsSpawnStronger() {
 		super( DifficultyModifier.DEFAULT, "MobsSpawnStronger", "All hostile mobs get damage and health bonuses." );
@@ -28,8 +30,9 @@ public class MobsSpawnStronger extends DifficultyModifier {
 		onSpawned.addCondition( new Condition.Excludable() )
 			.addCondition( data->data.level != null )
 			.addCondition( data->this.canMobAttack( data.target ) )
+			.addCondition( data->this.isNotDimensionExcluded( data.level ) )
 			.addCondition( data->this.isNotMobExcluded( data.target ) )
-			.addConfigs( this.healthBonus, this.damageBonus, this.nightMultiplier, this.excludedMobs );
+			.addConfigs( this.healthBonus, this.damageBonus, this.nightMultiplier, this.excludedMobs, this.excludedDimensions );
 
 		this.addContext( onSpawned );
 	}
@@ -48,7 +51,11 @@ public class MobsSpawnStronger extends DifficultyModifier {
 		return entity instanceof Mob && AttributeHandler.hasAttribute( entity, Attributes.ATTACK_DAMAGE );
 	}
 
+	private boolean isNotDimensionExcluded( ServerLevel level ) {
+		return !this.excludedDimensions.contains( Utility.getRegistryString( level ) );
+	}
+
 	private boolean isNotMobExcluded( LivingEntity entity ) {
-		return !this.excludedMobs.contains( EntityType.getKey( entity.getType() ).toString() );
+		return !this.excludedMobs.contains( Utility.getRegistryString( entity ) );
 	}
 }
