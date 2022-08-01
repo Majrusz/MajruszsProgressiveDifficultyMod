aa14d1e512b11c10dcdd82d8ebc37fdc87356ea6
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-08-01 21:08:39 +0200

    Undead mobs cannot take fall debuffs anymore

--- src/main/java/com/majruszsdifficulty/gamemodifiers/list/FallDebuffs.java
+++ src/main/java/com/majruszsdifficulty/gamemodifiers/list/FallDebuffs.java
@@ -9,6 +9,8 @@ import com.mlib.gamemodifiers.contexts.OnDamagedContext;
 import com.mlib.gamemodifiers.data.OnDamagedData;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobType;
 
 public class FallDebuffs extends DifficultyModifier {
 	final ProgressiveEffectConfig nausea = new ProgressiveEffectConfig( "Nausea", ()->MobEffects.CONFUSION, 0, 8.0 );
@@ -22,6 +24,7 @@ public class FallDebuffs extends DifficultyModifier {
 			.addCondition( new CustomConditions.CRDChance( 1.0, false ) )
 			.addCondition( new Condition.Excludable() )
 			.addCondition( data->data.source.equals( DamageSource.FALL ) && data.event.getAmount() > 2.0f )
+			.addCondition( data->!( data.target instanceof Mob mob ) || mob.getMobType() != MobType.UNDEAD )
 			.addConfigs( this.nausea, this.slowness );
 
 		this.addContext( onDamaged );
