f530af4b7438b0718d890960d356ed129d0e3703
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-11-12 14:58:03 +0100

    Reworked Bleeding Immunity and fixed bug with this effect not blocking Bleeding properly

--- src/main/java/com/majruszsdifficulty/effects/BleedingImmunityEffect.java
+++ src/main/java/com/majruszsdifficulty/effects/BleedingImmunityEffect.java
@@ -1,18 +1,18 @@
 package com.majruszsdifficulty.effects;
 
 import com.majruszsdifficulty.Registries;
+import com.mlib.annotations.AutoInstance;
+import com.mlib.gamemodifiers.Condition;
+import com.mlib.gamemodifiers.GameModifier;
+import com.mlib.gamemodifiers.contexts.OnEffectApplicable;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraftforge.event.entity.living.MobEffectEvent;
 import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 
 import javax.annotation.Nullable;
 
-@Mod.EventBusSubscriber
 public class BleedingImmunityEffect extends MobEffect {
 	public BleedingImmunityEffect() {
 		super( MobEffectCategory.BENEFICIAL, 0xff990000 );
@@ -22,7 +22,9 @@ public class BleedingImmunityEffect extends MobEffect {
 	public void applyEffectTick( LivingEntity entity, int amplifier ) {}
 
 	@Override
-	public void applyInstantenousEffect( @Nullable Entity source, @Nullable Entity indirectSource, LivingEntity entity, int amplifier, double health ) {
+	public void applyInstantenousEffect( @Nullable Entity source, @Nullable Entity indirectSource, LivingEntity entity,
+		int amplifier, double health
+	) {
 		entity.removeEffect( Registries.BLEEDING.get() );
 	}
 
@@ -31,9 +33,20 @@ public class BleedingImmunityEffect extends MobEffect {
 		return false;
 	}
 
-	@SubscribeEvent
-	public static void onEffectApplied( MobEffectEvent.Added event ) {
-		if( event.getEffectInstance().getEffect() instanceof BleedingEffect && event.getEntity().hasEffect( Registries.BLEEDING_IMMUNITY.get() ) )
-			event.setResult( Event.Result.DENY );
+	@AutoInstance
+	public static class BleedingImmunity extends GameModifier {
+		public BleedingImmunity() {
+			super( Registries.Modifiers.DEFAULT, "BleedingImmunity", "Config for Bleeding Immunity effect." );
+
+			OnEffectApplicable.Context onEffectApplicable = new OnEffectApplicable.Context( this::cancelBleeding );
+			onEffectApplicable.addCondition( new Condition.HasEffect( Registries.BLEEDING_IMMUNITY ) )
+				.addCondition( data->data.effect.equals( Registries.BLEEDING.get() ) );
+
+			this.addContexts( onEffectApplicable );
+		}
+
+		private void cancelBleeding( OnEffectApplicable.Data data ) {
+			data.event.setResult( Event.Result.DENY );
+		}
 	}
 }
