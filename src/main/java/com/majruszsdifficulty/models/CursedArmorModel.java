04b87ad774b97b8aad8df3855499ff0d8821a87c
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-17 16:29:00 +0100

    Added extra scale animation

--- src/main/java/com/majruszsdifficulty/models/CursedArmorModel.java
+++ src/main/java/com/majruszsdifficulty/models/CursedArmorModel.java
@@ -19,6 +19,9 @@ public class CursedArmorModel< Type extends CursedArmorEntity > extends Humanoid
 	static final Animation.Vector LEFT_ARM = new Animation.Vector( CursedArmorEntity.ASSEMBLE_DURATION );
 	static final Animation.Vector BODY = new Animation.Vector( CursedArmorEntity.ASSEMBLE_DURATION );
 	static final Animation.Vector HEAD = new Animation.Vector( CursedArmorEntity.ASSEMBLE_DURATION );
+	static final Animation.Float SCALE_1 = new Animation.Float( CursedArmorEntity.ASSEMBLE_DURATION );
+	static final Animation.Float SCALE_2 = new Animation.Float( CursedArmorEntity.ASSEMBLE_DURATION );
+	static final Animation.Float SCALE_3 = new Animation.Float( CursedArmorEntity.ASSEMBLE_DURATION );
 
 	static {
 		RIGHT_LEG.addNewVectorFrame( 0.0f, new Vector3f( 0.0f, 15.0f, 0.0f ) )
@@ -56,6 +59,21 @@ public class CursedArmorModel< Type extends CursedArmorEntity > extends Humanoid
 			.addNewVectorFrame( 3.5f, new Vector3f( 0.0f, -10.0f, 0.0f ), Frame.InterpolationType.SQUARE_ROOT )
 			.addNewVectorFrame( 3.75f, new Vector3f( 0.0f, -6.0f, 0.0f ), Frame.InterpolationType.SQUARE_ROOT )
 			.addNewVectorFrame( 4.5f, new Vector3f( 0.0f, 0.0f, 0.0f ), Frame.InterpolationType.SQUARE );
+
+		SCALE_1.addNewFloatFrame( 0.0f, 0.01f )
+			.addNewFloatFrame( 1.0f, 0.01f )
+			.addNewFloatFrame( 2.0f, 1.0f )
+			.addNewFloatFrame( 4.5f, 1.0f );
+
+		SCALE_2.addNewFloatFrame( 0.0f, 0.01f )
+			.addNewFloatFrame( 1.5f, 0.01f )
+			.addNewFloatFrame( 2.5f, 1.0f )
+			.addNewFloatFrame( 4.5f, 1.0f );
+
+		SCALE_3.addNewFloatFrame( 0.0f, 0.01f )
+			.addNewFloatFrame( 2.0f, 0.01f )
+			.addNewFloatFrame( 3.0f, 1.0f )
+			.addNewFloatFrame( 4.5f, 1.0f );
 	}
 
 	public CursedArmorModel( ModelPart root ) {
@@ -77,5 +95,17 @@ public class CursedArmorModel< Type extends CursedArmorEntity > extends Humanoid
 		Animation.addPosition( LEFT_ARM.apply( assembleTime, ageInTicks ), this.leftArm );
 		Animation.addPosition( BODY.apply( assembleTime, ageInTicks ), this.body );
 		Animation.addPosition( HEAD.apply( assembleTime, ageInTicks ), this.head );
+
+		float scale3 = SCALE_3.apply( assembleTime, ageInTicks );
+		Animation.applyScale( scale3, this.rightLeg );
+		Animation.applyScale( scale3, this.leftLeg );
+
+		float scale2 = SCALE_2.apply( assembleTime, ageInTicks );
+		Animation.applyScale( scale2, this.rightArm );
+		Animation.applyScale( scale2, this.leftArm );
+
+		float scale1 = SCALE_1.apply( assembleTime, ageInTicks );
+		Animation.applyScale( scale1, this.body );
+		Animation.applyScale( scale1, this.head );
 	}
 }
