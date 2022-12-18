f02fa62e28292d04620eec602fa133097a639d04
Majrusz <mateuszsmolarkiewicz@wp.pl>
2022-12-18 19:15:39 +0100

    Removed Haste bonus from all Enderium items and reworked till bonus

--- src/main/java/com/majruszsdifficulty/items/EnderiumHoeItem.java
+++ src/main/java/com/majruszsdifficulty/items/EnderiumHoeItem.java
@@ -1,11 +1,34 @@
 package com.majruszsdifficulty.items;
 
-import com.majruszsdifficulty.Registries;
+import com.mlib.annotations.AutoInstance;
+import com.mlib.gamemodifiers.contexts.OnFarmlandTillCheck;
+import com.mlib.gamemodifiers.contexts.OnItemAttributeTooltip;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.HoeItem;
 import net.minecraft.world.item.Rarity;
 
 public class EnderiumHoeItem extends HoeItem {
 	public EnderiumHoeItem() {
 		super( CustomItemTier.END, -5, 0.0f, new Properties().rarity( Rarity.UNCOMMON ).fireResistant() );
+	}
+
+	@AutoInstance
+	public static class IncreaseTillArea {
+		static final String ATTRIBUTE_ID = "item.majruszsdifficulty.enderium_hoe.till_range";
+
+		public IncreaseTillArea() {
+			new OnFarmlandTillCheck.Context( OnFarmlandTillCheck.INCREASE_AREA )
+				.addCondition( OnFarmlandTillCheck.IS_NOT_CROUCHING )
+				.addCondition( data->data.itemStack.getItem() instanceof EnderiumHoeItem );
+
+			new OnItemAttributeTooltip.Context( this::addTooltip )
+				.addCondition( data->data.item instanceof EnderiumHoeItem );
+		}
+
+		private void addTooltip( OnItemAttributeTooltip.Data data ) {
+			data.add( EquipmentSlot.MAINHAND, Component.translatable( ATTRIBUTE_ID ).withStyle( ChatFormatting.DARK_GREEN ) );
+		}
 	}
 }
