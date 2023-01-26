217b41e66d6c84fd26670b8ff687241c69cd772d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-01-26 19:07:05 +0100

    Used new SerializableStructure to save the data

--- src/main/java/com/majruszsdifficulty/undeadarmy/Keys.java
+++ src/main/java/com/majruszsdifficulty/undeadarmy/Keys.java
@@ -2,9 +2,11 @@ package com.majruszsdifficulty.undeadarmy;
 
 class Keys {
 	// Entity
-	public static final String POSITION = "undead_army_position";
 
 	// Undead Army
+	public static final String POSITION = "undead_army_position";
+	public static final String DIRECTION = "direction";
+	public static final String PHASE = "phase";
 	public static final String PHASE_TICKS_LEFT = "phase_ticks_left";
 	public static final String PHASE_TICKS_TOTAL = "phase_ticks_total";
 	public static final String CURRENT_WAVE = "current_wave";
