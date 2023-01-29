63e1b20309d2799d018ebe1937153954bd4017a6
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-01-29 21:30:00 +0100

    Made Undead Army mobs be highlighted after 5 minutes

--- src/main/java/com/majruszsdifficulty/undeadarmy/Config.java
+++ src/main/java/com/majruszsdifficulty/undeadarmy/Config.java
@@ -33,6 +33,7 @@ public class Config extends GameModifier {
 	private final BooleanConfig availability = new BooleanConfig( true );
 	private final DoubleConfig waveDuration = new DoubleConfig( 1200.0, new Range<>( 300.0, 3600.0 ) );
 	private final DoubleConfig preparationDuration = new DoubleConfig( 10.0, new Range<>( 4.0, 30.0 ) );
+	private final DoubleConfig highlightDelay = new DoubleConfig( 300.0, new Range<>( 30.0, 3600.0 ) );
 	private final DoubleConfig extraSizePerPlayer = new DoubleConfig( 0.5, new Range<>( 0.0, 1.0 ) );
 	private final IntegerConfig armyRadius = new IntegerConfig( 70, new Range<>( 35, 140 ) );
 	private WavesDef wavesDef = null;
@@ -52,6 +53,7 @@ public class Config extends GameModifier {
 		this.addConfig( this.availability.name( "is_enabled" ).comment( "Determines whether the Undead Army can spawn in any way." ) )
 			.addConfig( this.waveDuration.name( "wave_duration" ).comment( "Duration that players have to defeat a single wave (in seconds)." ) )
 			.addConfig( this.preparationDuration.name( "preparation_duration" ).comment( "Duration before the next wave arrives (in seconds)." ) )
+			.addConfig( this.highlightDelay.name( "highlight_delay" ).comment( "Duration before all mobs will be highlighted (in seconds)." ) )
 			.addConfig( this.extraSizePerPlayer.name( "extra_size_per_player" ).comment( "Extra size ratio per each additional player on multiplayer (0.25 means ~25% bigger army per player)." ) )
 			.addConfig( this.armyRadius.name( "army_radius" ).comment( "Radius, which determines how big is the raid circle (in blocks)." ) );
 	}
@@ -78,6 +80,10 @@ public class Config extends GameModifier {
 
 	public int getArmyRadius() {
 		return this.armyRadius.get();
+	}
+
+	public int getHighlightDelay() {
+		return this.highlightDelay.asTicks();
 	}
 
 	public int getSpawnRadius() {
