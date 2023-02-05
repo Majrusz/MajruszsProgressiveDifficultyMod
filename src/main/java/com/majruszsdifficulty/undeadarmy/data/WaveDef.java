e21db99535d7b32d8d476f726bea6bff6a0a7f01
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-02-05 10:46:54 +0100

    Added game stage check

--- src/main/java/com/majruszsdifficulty/undeadarmy/data/WaveDef.java
+++ src/main/java/com/majruszsdifficulty/undeadarmy/data/WaveDef.java
@@ -1,5 +1,6 @@
 package com.majruszsdifficulty.undeadarmy.data;
 
+import com.majruszsdifficulty.GameStage;
 import com.mlib.data.SerializableStructure;
 
 import java.util.ArrayList;
@@ -9,10 +10,12 @@ public class WaveDef extends SerializableStructure {
 	public final List< MobDef > mobDefs = new ArrayList<>();
 	public MobDef boss;
 	public int experience = 0;
+	public GameStage.Stage gameStage = GameStage.Stage.NORMAL;
 
 	public WaveDef() {
 		this.define( "mobs", ()->this.mobDefs, this.mobDefs::addAll, MobDef::new );
 		this.define( "boss", ()->this.boss, x->this.boss = x, MobDef::new );
 		this.define( "exp", ()->this.experience, x->this.experience = x );
+		this.define( "game_stage", ()->this.gameStage, x->this.gameStage = x, GameStage.Stage::values );
 	}
 }
