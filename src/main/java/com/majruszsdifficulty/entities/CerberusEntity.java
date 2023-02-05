727eaea08be8871866678c8ffa9fddbfac8c551e
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-02-05 13:03:09 +0100

    Added Cerberus mob

---
+++ src/main/java/com/majruszsdifficulty/entities/CerberusEntity.java
@@ -0,0 +1,104 @@
+package com.majruszsdifficulty.entities;
+
+import com.mlib.Random;
+import com.mlib.effects.SoundHandler;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.monster.warden.Warden;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.function.Supplier;
+
+public class CerberusEntity extends Monster {
+	public static Supplier< EntityType< CerberusEntity > > createSupplier() {
+		return ()->EntityType.Builder.of( CerberusEntity::new, MobCategory.MONSTER ).sized( 2.0f, 2.0f ).build( "cerberus" );
+	}
+
+	public static AttributeSupplier getAttributeMap() {
+		return Mob.createMobAttributes()
+			.add( Attributes.MAX_HEALTH, 140.0 )
+			.add( Attributes.MOVEMENT_SPEED, 0.25 )
+			.add( Attributes.ATTACK_DAMAGE, 8.0 )
+			.add( Attributes.FOLLOW_RANGE, 30.0 )
+			.add( Attributes.ATTACK_KNOCKBACK, 6.0 )
+			.add( Attributes.KNOCKBACK_RESISTANCE, 0.75 )
+			.build();
+	}
+
+	public CerberusEntity( EntityType< ? extends CerberusEntity > type, Level world ) {
+		super( type, world );
+		this.xpReward = 17;
+	}
+
+	@Override
+	protected void registerGoals() {
+		this.goalSelector.addGoal( 7, new WaterAvoidingRandomStrollGoal( this, 1.0 ) );
+		this.goalSelector.addGoal( 8, new LookAtPlayerGoal( this, Player.class, 8.0f ) );
+		this.goalSelector.addGoal( 8, new RandomLookAroundGoal( this ) );
+
+		this.targetSelector.addGoal( 1, new HurtByTargetGoal( this ) );
+		this.targetSelector.addGoal( 2, new NearestAttackableTargetGoal<>( this, Player.class, true ) );
+		this.targetSelector.addGoal( 3, new NearestAttackableTargetGoal<>( this, IronGolem.class, true ) );
+		this.targetSelector.addGoal( 3, new NearestAttackableTargetGoal<>( this, Warden.class, true ) );
+	}
+
+	@Override
+	public int getExperienceReward() {
+		return Random.nextInt( 17 );
+	}
+
+	@Override
+	protected SoundEvent getAmbientSound() {
+		return SoundEvents.SKELETON_AMBIENT;
+	}
+
+	@Override
+	public MobType getMobType() {
+		return MobType.UNDEAD;
+	}
+
+	@Override
+	protected float getStandingEyeHeight( Pose poseIn, EntityDimensions sizeIn ) {
+		return 2.35f;
+	}
+
+	@Override
+	protected SoundEvent getHurtSound( DamageSource damageSource ) {
+		return SoundEvents.SKELETON_HURT;
+	}
+
+	@Override
+	protected SoundEvent getDeathSound() {
+		return SoundEvents.SKELETON_DEATH;
+	}
+
+	@Override
+	protected void playStepSound( BlockPos blockPos, BlockState blockState ) {
+		this.playSound( SoundEvents.SKELETON_STEP, 0.15f, 1.0f );
+	}
+
+	@Override
+	public void playSound( SoundEvent sound, float volume, float pitch ) {
+		if( this.isSilent() ) {
+			return;
+		}
+		float randomizedVolume = SoundHandler.randomized( volume * 1.25f ).get();
+		float randomizedPitch = SoundHandler.randomized( pitch * 0.75f ).get();
+
+		this.level.playSound( null, this.getX(), this.getY(), this.getZ(), sound, this.getSoundSource(), randomizedVolume, randomizedPitch );
+	}
+}
