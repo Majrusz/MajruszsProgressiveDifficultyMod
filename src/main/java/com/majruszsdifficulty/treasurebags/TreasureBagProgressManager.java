b9fa4369aa4710d922e45b50849c9bb20ae8d44c
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-02-26 21:32:31 +0100

    Added base for new Treasure Bag progress manager

---
+++ src/main/java/com/majruszsdifficulty/treasurebags/TreasureBagProgressManager.java
@@ -0,0 +1,7 @@
+package com.majruszsdifficulty.treasurebags;
+
+import com.mlib.data.SerializableStructure;
+
+public class TreasureBagProgressManager extends SerializableStructure {
+	public TreasureBagProgressManager() {}
+}
