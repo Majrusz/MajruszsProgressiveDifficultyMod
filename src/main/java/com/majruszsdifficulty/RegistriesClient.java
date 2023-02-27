b5db8d41caad06e40f61a1aa5e27a60c7ffe52a5
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-02-27 21:35:15 +0100

    Added deprecated tags to remove class in the future

--- src/main/java/com/majruszsdifficulty/RegistriesClient.java
+++ src/main/java/com/majruszsdifficulty/RegistriesClient.java
@@ -14,6 +14,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.ForgeHooksClient;
 
+@Deprecated( forRemoval = true )
 @OnlyIn( Dist.CLIENT )
 public class RegistriesClient {
 	public static void setup() {
