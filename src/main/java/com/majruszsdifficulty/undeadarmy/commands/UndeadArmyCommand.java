0aa1e9649313079d6f9311216e1cce22d4be4067
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-03-21 11:17:13 +0100

    Moved Undead Army commands to proper package

--- src/main/java/com/majruszsdifficulty/undeadarmy/commands/UndeadArmyCommand.java
+++ src/main/java/com/majruszsdifficulty/undeadarmy/commands/UndeadArmyCommand.java
@@ -1,17 +1,17 @@
-package com.majruszsdifficulty.commands;
+package com.majruszsdifficulty.undeadarmy.commands;
 
 import com.majruszsdifficulty.Registries;
 import com.majruszsdifficulty.undeadarmy.UndeadArmy;
+import com.mlib.commands.Command;
 import com.mlib.commands.CommandData;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.phys.Vec3;
 
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
-public class UndeadArmyCommand extends DifficultyCommand {
+public class UndeadArmyCommand extends Command {
 	final BiFunction< UndeadArmy, BlockPos, Component > componentSupplier;
 
 	public UndeadArmyCommand( String command, BiFunction< UndeadArmy, BlockPos, Component > componentSupplier ) {
@@ -29,7 +29,7 @@ public class UndeadArmyCommand extends DifficultyCommand {
 	public UndeadArmyCommand( String command, String successId, Consumer< UndeadArmy > consumer ) {
 		this( command, ( undeadArmy, position )->{
 			consumer.accept( undeadArmy );
-			return Component.translatable( "commands.undeadarmy." + successId, asVec3i( position ) );
+			return Component.translatable( "commands.undeadarmy." + successId, String.format( "(%s)", position.toShortString() ) );
 		} );
 	}
 
@@ -41,7 +41,7 @@ public class UndeadArmyCommand extends DifficultyCommand {
 			return 0;
 		}
 
-		data.source.sendSuccess( Component.translatable( "commands.undeadarmy.missing", asVec3i( position ) ), true );
+		data.source.sendSuccess( Component.translatable( "commands.undeadarmy.missing", String.format( "(%s)", position.toShortString() ) ), true );
 		return -1;
 	}
 }
--- src/main/java/com/majruszsdifficulty/undeadarmy/commands/UndeadArmyStartCommand.java
+++ src/main/java/com/majruszsdifficulty/undeadarmy/commands/UndeadArmyStartCommand.java
@@ -1,18 +1,18 @@
-package com.majruszsdifficulty.commands;
+package com.majruszsdifficulty.undeadarmy.commands;
 
 import com.majruszsdifficulty.Registries;
 import com.majruszsdifficulty.undeadarmy.data.Direction;
 import com.mlib.annotations.AutoInstance;
+import com.mlib.commands.Command;
 import com.mlib.commands.CommandData;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.phys.Vec3;
 
 import java.util.Optional;
 
 @AutoInstance
-public class UndeadArmyStartCommand extends DifficultyCommand {
+public class UndeadArmyStartCommand extends Command {
 	public UndeadArmyStartCommand() {
 		this.newBuilder()
 			.literal( "undeadarmy" )
@@ -29,11 +29,11 @@ public class UndeadArmyStartCommand extends DifficultyCommand {
 		Optional< Direction > direction = this.getOptionalEnumeration( data, Direction.class );
 		BlockPos position = this.getOptionalEntityOrPlayer( data ).blockPosition();
 		if( Registries.getUndeadArmyManager().tryToSpawn( new BlockPos( position ), direction ) ) {
-			data.source.sendSuccess( Component.translatable( "commands.undeadarmy.started", asVec3i( position ) ), true );
+			data.source.sendSuccess( Component.translatable( "commands.undeadarmy.started", String.format( "(%s)", position.toShortString() ) ), true );
 			return 0;
 		}
 
-		data.source.sendSuccess( Component.translatable( "commands.undeadarmy.cannot_start", asVec3i( position ) ), true );
+		data.source.sendSuccess( Component.translatable( "commands.undeadarmy.cannot_start", String.format( "(%s)", position.toShortString() ) ), true );
 		return -1;
 	}
 }
