5bab368ef4b1ff135f9d0339ea6716c0f73c1b25
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-06-29 23:02:23 +0200

    Reworked Bite Bleeding

--- src/main/java/com/majruszsdifficulty/gamemodifiers/list/bleeding/BiteBleeding.java
+++ src/main/java/com/majruszsdifficulty/gamemodifiers/list/bleeding/BiteBleeding.java
@@ -1,39 +1,39 @@
-package com.majruszsdifficulty.gamemodifiers.list;
+package com.majruszsdifficulty.gamemodifiers.list.bleeding;
 
-import com.majruszsdifficulty.gamestage.GameStage;
 import com.majruszsdifficulty.Registries;
 import com.majruszsdifficulty.gamemodifiers.CustomConditions;
 import com.majruszsdifficulty.gamemodifiers.contexts.OnBleedingCheck;
+import com.majruszsdifficulty.gamestage.GameStage;
 import com.mlib.annotations.AutoInstance;
 import com.mlib.config.ConfigGroup;
 import com.mlib.gamemodifiers.Condition;
 import com.mlib.gamemodifiers.ModConfigs;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.horse.Llama;
 import net.minecraft.world.entity.monster.Spider;
 import net.minecraft.world.entity.monster.Zombie;
 
-import javax.annotation.Nullable;
-
 @AutoInstance
 public class BiteBleeding {
 	public BiteBleeding() {
-		ConfigGroup group = ModConfigs.registerSubgroup( Registries.Groups.DEFAULT )
-			.name( "BiteBleeding" )
-			.comment( "Animals (wolfs and from other mods), zombies and spiders may inflict bleeding." );
+		ConfigGroup group = ModConfigs.registerSubgroup( Registries.Groups.BLEEDING )
+			.name( "Bite" )
+			.comment( "Wolves, zombies, spiders, and animals from other mods may inflict bleeding." );
 
 		OnBleedingCheck.listen( OnBleedingCheck.Data::trigger )
 			.addCondition( CustomConditions.gameStageAtLeast( GameStage.NORMAL ) )
 			.addCondition( Condition.chanceCRD( 0.5, false ) )
 			.addCondition( Condition.excludable() )
 			.addCondition( Condition.isLivingBeing( data->data.target ) )
-			.addCondition( Condition.predicate( data->canBite( data.attacker ) ) )
-			.addCondition( Condition.predicate( data->data.source.getDirectEntity() == data.attacker ) )
+			.addCondition( Condition.predicate( OnBleedingCheck.Data::isDirect ) )
+			.addCondition( BiteBleeding.canBite() )
 			.insertTo( group );
 	}
 
-	private static boolean canBite( @Nullable LivingEntity attacker ) {
-		return ( attacker instanceof Animal || attacker instanceof Zombie || attacker instanceof Spider ) && !( attacker instanceof Llama );
+	private static Condition< OnBleedingCheck.Data > canBite() {
+		return Condition.predicate( data->{
+			return ( data.attacker instanceof Animal || data.attacker instanceof Zombie || data.attacker instanceof Spider )
+				&& !( data.attacker instanceof Llama );
+		} );
 	}
 }
