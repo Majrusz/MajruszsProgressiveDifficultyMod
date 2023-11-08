b0138891577c340539d7852246d18771359d478d
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-08 20:08:52 +0100

    Readded Infested End Stone

--- common/src/main/java/com/majruszsdifficulty/MajruszsDifficulty.java
+++ common/src/main/java/com/majruszsdifficulty/MajruszsDifficulty.java
@@ -1,13 +1,16 @@
 package com.majruszsdifficulty;
 
+import com.majruszsdifficulty.blocks.InfestedEndStone;
 import com.majruszsdifficulty.data.Config;
 import com.majruszsdifficulty.data.WorldData;
 import com.majruszsdifficulty.gamestage.GameStageAdvancement;
 import com.majruszsdifficulty.items.FakeItem;
 import com.mlib.modhelper.ModHelper;
 import com.mlib.registry.RegistryGroup;
+import com.mlib.registry.RegistryObject;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 
 public class MajruszsDifficulty {
 	public static final String MOD_ID = "majruszsdifficulty";
@@ -18,7 +21,14 @@ public class MajruszsDifficulty {
 	public static final WorldData WORLD_DATA = HELPER.saved( WorldData::new );
 
 	// Registry Groups
+	public static final RegistryGroup< Block > BLOCKS = HELPER.create( BuiltInRegistries.BLOCK );
 	public static final RegistryGroup< Item > ITEMS = HELPER.create( BuiltInRegistries.ITEM );
+
+	// Blocks
+	public static final RegistryObject< InfestedEndStone > INFESTED_END_STONE = BLOCKS.create( "infested_end_stone", InfestedEndStone::new );
+
+	// Items
+	public static final RegistryObject< InfestedEndStone.Item > INFESTED_END_STONE_ITEM = ITEMS.create( "infested_end_stone", InfestedEndStone.Item::new );
 
 	// Items (fake)
 	static {
---
+++ common/src/main/java/com/majruszsdifficulty/blocks/InfestedEndStone.java
@@ -0,0 +1,65 @@
+package com.majruszsdifficulty.blocks;
+
+import com.majruszsdifficulty.MajruszsDifficulty;
+import com.mlib.entity.EntityHelper;
+import com.mlib.math.AnyPos;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.MapColor;
+
+public class InfestedEndStone extends Block {
+	public InfestedEndStone() {
+		super( Properties.of().mapColor( MapColor.SAND ).strength( 0.0f, 0.75f ).sound( SoundType.STONE ) );
+	}
+
+	@Override
+	public void spawnAfterBreak( BlockState blockState, ServerLevel level, BlockPos blockPos, ItemStack itemStack, boolean p_221364_ ) {
+		super.spawnAfterBreak( blockState, level, blockPos, itemStack, p_221364_ );
+
+		this.tryToSpawnEndermite( level, itemStack, blockPos );
+	}
+
+	@Override
+	public void wasExploded( Level level, BlockPos blockPos, Explosion explosion ) {
+		super.wasExploded( level, blockPos, explosion );
+
+		this.tryToSpawnEndermite( level, ItemStack.EMPTY, blockPos );
+	}
+
+	private void tryToSpawnEndermite( Level level, ItemStack itemStack, BlockPos blockPos ) {
+		if( !( level instanceof ServerLevel ) ) {
+			return;
+		}
+
+		if( !level.getGameRules().getBoolean( GameRules.RULE_DOBLOCKDROPS ) ) {
+			return;
+		}
+
+		if( EnchantmentHelper.getItemEnchantmentLevel( Enchantments.SILK_TOUCH, itemStack ) != 0 ) {
+			return;
+		}
+
+		EntityHelper.createSpawner( ()->EntityType.ENDERMITE, level )
+			.position( AnyPos.from( blockPos ).center().vec3() )
+			.beforeEvent( Mob::spawnAnim )
+			.spawn();
+	}
+
+	public static class Item extends BlockItem {
+		public Item() {
+			super( MajruszsDifficulty.INFESTED_END_STONE.get(), new Properties().stacksTo( 64 ) );
+		}
+	}
+}
