73e3707a49008d15c9d014e92fc60e5816a95b43
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-25 20:15:18 +0100

    Increased bleeding chance bonuses from enchantments

--- common/src/main/java/com/majruszsdifficulty/effects/bleeding/BleedingSources.java
+++ common/src/main/java/com/majruszsdifficulty/effects/bleeding/BleedingSources.java
@@ -96,15 +96,15 @@ public class BleedingSources {
 		private static boolean IS_ENABLED = true;
 		private static List< ToolDef > TOOL_DEFS = List.of(
 			new ToolDef( "minecraft:trident", 0.3f, List.of(
-				new EnchantmentDef( "minecraft:impaling", 0.02f )
+				new EnchantmentDef( "minecraft:impaling", 0.04f )
 			) ),
 			new ToolDef( "{regex}.*_sword", 0.3f, List.of(
-				new EnchantmentDef( "minecraft:sharpness", 0.02f ),
-				new EnchantmentDef( "{regex}(minecraft:smite)|(minecraft:bane_of_arthropods)|(majruszsenchantments:misanthropy)", 0.01f )
+				new EnchantmentDef( "minecraft:sharpness", 0.04f ),
+				new EnchantmentDef( "{regex}(minecraft:smite)|(minecraft:bane_of_arthropods)|(majruszsenchantments:misanthropy)", 0.02f )
 			) ),
 			new ToolDef( "{regex}.*_axe", 0.26f, List.of(
-				new EnchantmentDef( "minecraft:sharpness", 0.02f ),
-				new EnchantmentDef( "{regex}(minecraft:smite)|(minecraft:bane_of_arthropods)|(majruszsenchantments:misanthropy)", 0.01f )
+				new EnchantmentDef( "minecraft:sharpness", 0.04f ),
+				new EnchantmentDef( "{regex}(minecraft:smite)|(minecraft:bane_of_arthropods)|(majruszsenchantments:misanthropy)", 0.02f )
 			) ),
 			new ToolDef( "{regex}(.*_pickaxe)|minecraft:shears", 0.22f ),
 			new ToolDef( "{regex}.*_shovel", 0.18f ),
