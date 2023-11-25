96a19dd029e50d5ae6619cf9a056869828f24c9b
Majrusz <mateuszsmolarkiewicz@wp.pl>
2023-11-25 09:57:34 +0100

    Fixed spawn rate increaser to only affect monsters

--- common/src/main/java/com/majruszsdifficulty/bloodmoon/listeners/SpawnRateIncreaser.java
+++ common/src/main/java/com/majruszsdifficulty/bloodmoon/listeners/SpawnRateIncreaser.java
@@ -4,14 +4,17 @@ import com.majruszlibrary.events.OnMobSpawnLimitGet;
 import com.majruszlibrary.events.OnMobSpawnRateGet;
 import com.majruszsdifficulty.bloodmoon.BloodMoonConfig;
 import com.majruszsdifficulty.bloodmoon.BloodMoonHelper;
+import net.minecraft.world.entity.MobCategory;
 
 public class SpawnRateIncreaser {
 	static {
 		OnMobSpawnRateGet.listen( SpawnRateIncreaser::increase )
-			.addCondition( data->BloodMoonHelper.isActive() );
+			.addCondition( data->BloodMoonHelper.isActive() )
+			.addCondition( data->data.category.equals( MobCategory.MONSTER ) );
 
 		OnMobSpawnLimitGet.listen( SpawnRateIncreaser::increase )
-			.addCondition( data->BloodMoonHelper.isActive() );
+			.addCondition( data->BloodMoonHelper.isActive() )
+			.addCondition( data->data.category.equals( MobCategory.MONSTER ) );
 	}
 
 	private static void increase( OnMobSpawnRateGet data ) {
